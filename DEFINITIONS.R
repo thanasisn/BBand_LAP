dd7eddc350cbaf2a80c115b424eb32b40e498195
thanasisn <11601717+thanasisn@users.noreply.github.com>
2023-03-19 08:07:24 +0200

    Commit 2023-03-19 08:07

--- DEFINITIONS.R
+++ DEFINITIONS.R
@@ -10,17 +10,18 @@ BASED      <- "~/BBand_LAP/"
 SUN_FOLDER <- "~/DATA_RAW/SUN/PySolar_LAP/"
 SIRENA_GLB <- "~/DATA_RAW/Bband/AC21_LAP.GLB/"
 SIRENA_DIR <- "~/DATA_RAW/Bband/CHP1_lap.DIR/"
-
-RADMON_DIR <- "~/DATA_RAW/Raddata/3"
 trSYNC_DIR <- "~/DATA_RAW/tracker_chp1/tracker_SYNC"
 CHPTMP_DIR <- "~/DATA_RAW/tracker_chp1/Tracker_THERM"
+SIRENA_TOT <- "~/DATA/cm21_data_validation/AC21_lap.GLB_TOT"
+
+RADMON_DIR <- "~/DATA_RAW/Raddata/3"
 
 
 ####    DB specifications    ###################################################
 DB_DIR        <- "~/ZHOST/Broad_Band_BD"
 DB_lock       <- "~/ZHOST/Broad_Band_BD.stopfile"
 DB_META_fl    <- "~/ZHOST/Broad_Band_metadata.parquet"
-# DB_start_date <- as.Date("1993-04-01")
+# DB_start_date <- as.Date("1991-01-01")   ## <- start in 1993-01-19!!!!!
 DB_start_date <- as.Date("2016-01-01")
 
 
