8fab192b031bae2512edd7dac8f1ebed4901015a
thanasisn <11601717+thanasisn@users.noreply.github.com>
2023-04-02 08:13:11 +0300

    Commit 2023-04-02 08:13

--- DEFINITIONS.R
+++ DEFINITIONS.R
@@ -41,7 +41,7 @@ CHP_TEMP_UNC_LIM  <-   0.8  ## Drop temperatures with uncertainty above this val
 
 
 ####   Dark Calculations and definitions    ####################################
-DSTRETCH    <-  20 * 3600  ## Extend of dark signal for morning and evening of the same day (R30)
+DSTRETCH    <-   3 * 3600  ## Extend of dark signal for morning and evening of the same day (R30)
 DCOUNTLIM   <-  10         ## Number of valid measurements to compute dark (R30)
 DARK_ELEV   <- -10         ## Sun elevation limit to get dark signal (R20, R30)
 MINLIMnight <- -15         ## Lower radiation limit  when dark       (R20) -> ToolowDark
