96d55cae01a82dc7e692f4f11ebe05ceb6e065f6
thanasisn <11601717+thanasisn@users.noreply.github.com>
2023-04-04 12:13:10 +0300

    Commit 2023-04-04 12:13

--- build_db/Build_BB_DB.R
+++ build_db/Build_BB_DB.R
@@ -24,7 +24,7 @@ source("~/BBand_LAP/build_db/Build_DB_03_chp1.R"            )
 source("~/BBand_LAP/build_db/Build_DB_04_chp1_SNC.R"        )
 source("~/BBand_LAP/build_db/Build_DB_05_chp1_TMP.R"        )
 source("~/BBand_LAP/build_db/Build_DB_06_cm21_TOT.R"        )
-## Flag data bad data
+## Flag bad data
 source("~/BBand_LAP/build_db/Build_DB_30_exclude_ranges.R"  )
 ## Apply dark offset and convert to radiation
 source("~/BBand_LAP/build_db/Build_DB_42_cm21_dark_radiat.R")
