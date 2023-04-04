14a6a87a870475b8ee77ddbc124dc3036c3c349f
thanasisn <11601717+thanasisn@users.noreply.github.com>
2023-04-04 15:07:36 +0300

    Commit 2023-04-04 15:07

---
+++ inspect_db/Plot_daily_CHP1_sig.R
@@ -0,0 +1,135 @@
+# /* !/usr/bin/env Rscript */
+# /* Copyright (C) 2022-2023 Athanasios Natsis <natsisphysicist@gmail.com> */
+#' ---
+#' title:         "Inspect raw CHP-1 data **SIG** "
+#' author:        "Natsis Athanasios"
+#' institute:     "AUTH"
+#' affiliation:   "Laboratory of Atmospheric Physics"
+#' abstract:      "Inspect raw data from CHP1."
+#' documentclass: article
+#' classoption:   a4paper,oneside
+#' fontsize:      10pt
+#' geometry:      "left=0.5in,right=0.5in,top=0.5in,bottom=0.5in"
+#'
+#' link-citations:  yes
+#' colorlinks:      yes
+#'
+#' header-includes:
+#' - \usepackage{caption}
+#' - \usepackage{placeins}
+#' - \captionsetup{font=small}
+#'
+#' output:
+#'   bookdown::pdf_document2:
+#'     number_sections:  no
+#'     fig_caption:      no
+#'     keep_tex:         no
+#'     latex_engine:     xelatex
+#'     toc:              yes
+#'     fig_width:        8
+#'     fig_height:       5
+#'   html_document:
+#'     toc:        true
+#'     fig_width:  7.5
+#'     fig_height: 5
+#'
+#' date: "`r format(Sys.time(), '%F')`"
+#'
+#' ---
+
+#'
+#'  **SIG**
+#'
+#' **Source code: [`github.com/thanasisn/BBand_LAP`](`https://github.com/thanasisn/BBand_LAP`)**
+#'
+#' **Data display: [`thanasisn.netlify.app/3-data_display`](`https://thanasisn.netlify.app/3-data_display`)**
+#'
+#'
+#+ echo=F, include=T
+
+
+#+ echo=F, include=F
+## __ Document options ---------------------------------------------------------
+knitr::opts_chunk$set(comment    = ""       )
+knitr::opts_chunk$set(dev        = "png"    )
+knitr::opts_chunk$set(out.width  = "100%"   )
+knitr::opts_chunk$set(fig.align  = "center" )
+knitr::opts_chunk$set(fig.pos    = '!h'     )
+
+
+## __ Set environment  ---------------------------------------------------------
+Sys.setenv(TZ = "UTC")
+tic <- Sys.time()
+Script.Name <- "~/BBand_LAP/inspect_db/Plot_daily_CHP1_sig.R"
+
+source("~/BBand_LAP/DEFINITIONS.R")
+source("~/BBand_LAP/functions/Functions_BBand_LAP.R")
+source("~/CODE/FUNCTIONS/R/execlock.R")
+# mylock(DB_lock)
+
+
+if (!interactive()) {
+    pdf( file = paste0("~/BBand_LAP/RUNTIME/", basename(sub("\\.R$", ".pdf", Script.Name))))
+    sink(file = paste0("~/BBand_LAP/RUNTIME/", basename(sub("\\.R$", ".out", Script.Name))), split = TRUE)
+}
+
+library(arrow,      warn.conflicts = TRUE, quietly = TRUE)
+library(dplyr,      warn.conflicts = TRUE, quietly = TRUE)
+library(data.table, warn.conflicts = TRUE, quietly = TRUE)
+library(lubridate,  warn.conflicts = TRUE, quietly = TRUE)
+
+
+## __  Variables  --------------------------------------------------------------
+OUT_FOLDER <- "~/BBand_LAP/REPORTS/DAILY/CHP1_signal/"
+OUT_PREFIX <- "CHP1_signal_"
+
+
+
+## __ Execution control  -------------------------------------------------------
+BB_meta   <- read_parquet(DB_META_fl)
+
+## get years to plot by change date?
+
+BB_meta |> select(matches("Day|chp1")) |> filter(!is.na(chp1_basename)) |> select("day", where(is.POSIXct))
+
+
+stop()
+
+## years in the data base
+datayears <- opendata() |> select(year) |> unique() |> collect() |> pull()
+
+
+
+## TODO compare output files with parsed dates from meta
+years_to_do <- datayears
+
+# TEST
+# years_to_do <- 2016
+
+
+#+ include=TRUE, echo=FALSE, results="asis"
+for (YYYY in years_to_do) {
+    days_of_year <- seq.Date(as.Date(paste0(YYYY, "-01-01")),
+                             as.Date(paste0(YYYY, "-12-31")), by = "day")
+    ## don't go to the future
+    days_of_year <- days_of_year[days_of_year <= Sys.Date()]
+
+    ## load data for year
+    year_data <- data.table(opendata() |> filter(year == YYYY) |> collect())
+
+
+
+
+
+
+}
+
+
+
+
+
+#' **END**
+#+ include=T, echo=F
+# myunlock(DB_lock)
+tac <- Sys.time()
+cat(sprintf("%s %s@%s %s %f mins\n\n",Sys.time(),Sys.info()["login"],Sys.info()["nodename"],Script.Name,difftime(tac,tic,units="mins")))
