bb13406e5a5dd008ba477f18ef2a79ea3d3e46dd
thanasisn <11601717+thanasisn@users.noreply.github.com>
2023-04-04 09:07:27 +0300

    Commit 2023-04-04 09:07

---
+++ process/Build_DB_60_chp1_dark.R
@@ -0,0 +1,230 @@
+#!/opt/R/4.2.3/bin/Rscript
+# /* Copyright (C) 2022-2023 Athanasios Natsis <natsisphysicist@gmail.com> */
+
+#'
+#' TODO:
+#'
+#' - Darck and radiation for CHP-1 and CM-21
+#'
+#'
+#+ include=T, echo=F
+
+## __ Set environment  ---------------------------------------------------------
+rm(list = (ls()[ls() != ""]))
+Sys.setenv(TZ = "UTC")
+tic <- Sys.time()
+Script.Name <- "~/BBand_LAP/settname.R"
+
+source("~/BBand_LAP/DEFINITIONS.R")
+source("~/BBand_LAP/functions/Functions_CHP1.R")
+source("~/BBand_LAP/functions/Functions_dark_calculation.R")
+source("~/BBand_LAP/functions/Functions_BBand_LAP.R")
+source("~/CODE/FUNCTIONS/R/execlock.R")
+# mylock(DB_lock)
+
+if (!interactive()) {
+    pdf( file = paste0("~/BBand_LAP/RUNTIME/", basename(sub("\\.R$", ".pdf", Script.Name))))
+    sink(file = paste0("~/BBand_LAP/RUNTIME/", basename(sub("\\.R$", ".out", Script.Name))), split = TRUE)
+}
+
+library(arrow,      warn.conflicts = TRUE, quietly = TRUE)
+library(dplyr,      warn.conflicts = TRUE, quietly = TRUE)
+library(lubridate,  warn.conflicts = TRUE, quietly = TRUE)
+library(data.table, warn.conflicts = TRUE, quietly = TRUE)
+library(tools,      warn.conflicts = TRUE, quietly = TRUE)
+library(pander,     warn.conflicts = TRUE, quietly = TRUE)
+
+
+
+##  Initialize meta data file  -------------------------------------------------
+if (file.exists(DB_META_fl)) {
+    BB_meta <- read_parquet(DB_META_fl)
+    ## add more days
+    BB_meta <- merge(BB_meta,
+                     data.table(day = seq(max(BB_meta$day),
+                                          Sys.Date(),
+                                          by = "day")),
+                     by = "day",
+                     all = TRUE)
+    stopifnot(sum(duplicated(BB_meta$day)) == 0)
+    ## new columns
+    var <- "chp1_dark_flag"
+    if (!any(names(BB_meta) == var)) {
+        BB_meta[[var]] <- as.character(NA)
+    }
+} else {
+    stop("NO METADATA FILE!!")
+}
+
+
+
+##  Dark calculations on dataset  ----------------------------------------------
+
+## list data base files
+filelist <- data.table(
+    names = list.files(DB_DIR,
+                       pattern = "*.parquet",
+                       recursive  = TRUE,
+                       full.names = TRUE))
+dd      <- dirname(filelist$names)
+dd      <- tstrsplit(dd, "/")
+
+filelist$flmonth <- as.numeric(unlist(dd[length(dd)]))
+filelist$flyear  <- as.numeric(unlist(dd[length(dd)-1]))
+
+## list data set files to touch
+todosets <- unique(rbind(
+    BB_meta[is.na(chp1_dark_flag),
+            .(month = month(day), year = year(day))]
+))
+
+
+## to do check done
+
+## select what to touch
+filelist <- filelist[todosets, on = .(flmonth = month, flyear = year)]
+rm(todosets, dd)
+
+
+## loop data base files computing black for CHP-1
+for (af in filelist$names) {
+    datapart <- data.table(read_parquet(af))
+    cat("Load: ", af, "\n")
+
+    ## Ignore bad and missing data
+    usedata <- datapart[is.na(chp1_bad_data_flag) & !is.na(CHP1_sig) ]
+    if (nrow(usedata) == 0) {
+        cat("\nNo usefull CHP-1 data in this file\n\n")
+        next()
+    }
+
+    ## loop days
+    for (aday in unique(as.Date(usedata$Date))) {
+        daydata <- usedata[ as.Date(Date) == aday ]
+
+        # dark_day <- dark_calculations(
+            dates      = daydata$Date
+            values     = daydata$CM21_sig
+            elevatio   = daydata$Eleva
+            nightlimit = DARK_ELEV
+            dstretch   = DSTRETCH
+        # )
+
+
+        ## suppress warnings zoo::index
+        suppressWarnings({
+
+            ## find local noun
+            nounindex    <- which.max(elevatio)
+            ## split day in half
+            selectmorn   <- elevatio < nightlimit & index(elevatio) < nounindex
+            selecteven   <- elevatio < nightlimit & index(elevatio) > nounindex
+            ## all morning and evening dates
+            morning      <- dates[selectmorn]
+            evening      <- dates[selecteven]
+
+            ## morning selection with time limit
+            mornigend    <- morning[max(index(morning))]
+            mornigstart  <- mornigend - dstretch
+            ## selection for morning dark
+            morningdark  <- selectmorn & dates <= mornigend    & mornigstart < dates
+
+            ## evening selection with time limit
+            if (length(index(evening)) == 0) {
+                ii <- NA
+            } else {
+                ii <- index(evening)
+            }
+            eveningstart <- evening[min(ii)]
+            eveningend   <- eveningstart + dstretch
+            ## selection for evening dark
+            eveningdark  <- selecteven & dates >= eveningstart & dates < eveningend
+        })
+
+        return(
+            data.frame(
+                Mavg = mean(      values[morningdark],  na.rm = TRUE ),
+                Mmed = median(    values[morningdark],  na.rm = TRUE ),
+                Msta = max(       dates[ morningdark],  na.rm = TRUE ),
+                Mend = min(       dates[ morningdark],  na.rm = TRUE ),
+                Mcnt = sum(!is.na(values[morningdark])),
+                Eavg = mean(      values[eveningdark],  na.rm = TRUE ),
+                Emed = median(    values[eveningdark],  na.rm = TRUE ),
+                Esta = min(       dates[ eveningdark],  na.rm = TRUE ),
+                Eend = max(       dates[ eveningdark],  na.rm = TRUE ),
+                Ecnt = sum(!is.na(values[eveningdark]))
+            )
+        )
+
+        stop()
+    }
+
+
+
+    ## ignore data!!
+    grep("chp1", names(usedata), ignore.case = TRUE, value = TRUE)
+
+
+    stop()
+
+}
+
+# ####    Calculate Dark signal   ########################################
+# dark_day <- dark_calculations( dates      = daydata$Date,
+#                                values     = daydata$CM21value,
+#                                elevatio   = daydata$Eleva,
+#                                nightlimit = DARK_ELEV,
+#                                dstretch   = DSTRETCH)
+#
+#
+#
+# # if ( is.na(dark_day$Mmed) & is.na(dark_day$Emed) ) {
+# if ( ! ((!is.na(dark_day$Mmed) & dark_day$Mcnt >= DCOUNTLIM) |
+#         (!is.na(dark_day$Emed) & dark_day$Ecnt >= DCOUNTLIM)) ) {
+#     # cat("Can not apply dark\n")
+#     todays_dark_correction <- NA
+#     dark_flag              <- "MISSING"
+#     missingdark            <- NA
+#
+#     ## get dark from pre-computed file
+#     if (exists("construct")) {
+#         ## can not find date
+#         if (! theday %in% construct$Date) {
+#             todays_dark_correction <- NA
+#             dark_flag              <- "MISSING"
+#             missingdark            <- NA
+#         } else {
+#             ## get data from recomputed dark database
+#             todays_dark_correction <- construct[ Date == theday, DARK]
+#             dark_flag              <- "CONSTRUCTED"
+#         }
+#     }
+# } else {
+#     ####    Dark Correction function   #################################
+#     dark_generator <- dark_function(dark_day    = dark_day,
+#                                     DCOUNTLIM   = DCOUNTLIM,
+#                                     type        = "median",
+#                                     adate       = theday ,
+#                                     test        = test,
+#                                     missfiles   = missfiles,
+#                                     missingdark = missingdark )
+#
+#     ####    Create dark signal for correction    #######################
+#     todays_dark_correction <- dark_generator(daydata$Date)
+#     dark_flag              <- "COMPUTED"
+# }
+#
+# ####    Apply dark correction    #######################################
+# daydata[, CM21valueWdark := CM21value - todays_dark_correction ]
+
+
+
+
+
+
+
+
+
+myunlock(DB_lock)
+tac <- Sys.time()
+cat(sprintf("%s %s@%s %s %f mins\n\n",Sys.time(),Sys.info()["login"],Sys.info()["nodename"],Script.Name,difftime(tac,tic,units="mins")))
