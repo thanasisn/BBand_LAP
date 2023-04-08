1b67bb09594c1f996e01918cc5517767e6a1af19
thanasisn <11601717+thanasisn@users.noreply.github.com>
2023-04-08 16:13:10 +0300

    Commit 2023-04-08 16:13

--- build_db/Build_DB_01_pysolar.R
+++ build_db/Build_DB_01_pysolar.R
@@ -13,9 +13,25 @@
 #'  - SZA
 #'  - preNoon
 #'
+#'
+#' **Details and source code: [`github.com/thanasisn/BBand_LAP`](https://github.com/thanasisn/BBand_LAP)**
+#'
+#' **Data display: [`thanasisn.netlify.app/3-data_display`](https://thanasisn.netlify.app/3-data_display)**
+#'
+#'
+#+ echo=F, include=T
+
+
+#+ echo=F, include=F
+## __ Document options ---------------------------------------------------------
+knitr::opts_chunk$set(comment    = ""       )
+knitr::opts_chunk$set(dev        = "png"    )
+knitr::opts_chunk$set(out.width  = "100%"   )
+knitr::opts_chunk$set(fig.align  = "center" )
+knitr::opts_chunk$set(fig.pos    = '!h'     )
+
 
 ## __ Set environment  ---------------------------------------------------------
-rm(list = (ls()[ls() != ""]))
 Sys.setenv(TZ = "UTC")
 tic <- Sys.time()
 Script.Name <- "~/BBand_LAP/build_db/Build_DB_01_pysolar.R"
