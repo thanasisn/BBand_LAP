40f6afb8f91047d23d632f428d6c91a143ca897d
thanasisn <11601717+thanasisn@users.noreply.github.com>
2023-05-06 19:07:24 +0300

    Commit 2023-05-06 19:07

--- process/Export_CHP1_DIR.R
+++ process/Export_CHP1_DIR.R
@@ -43,22 +43,20 @@ options(max.print = 1500)
 
 
 ## data export folder
-DATOUT = "/home/athan/DATA/CHP1_LAP.DIR/"
+DATOUT     <- "~/DATA/CHP1_LAP.DIR/"
+OTHEREXPOR <- "~/ZHOST/"
 
+MAX_DATE <- as.POSIXct("2023-04-01")
+MIN_DATE <- as.POSIXct("2016-01-01")
 
-YEARS  <- c(2016,2017,2018,2019,2020,2021,2022)
-
-
-wecare <- c("SZA", "Date30", "wattDIR", "wattDIR_sds")
-# wecare <- c("SZA", "Date30", "wattDIR", "wattDIR_sds", "Azimuth")
-tag    <- paste0("Natsis Athanasios LAP AUTH ", strftime(Sys.time(), format = "%b %Y" ))
+tag <- paste0("Natsis Athanasios LAP AUTH ", strftime(Sys.time(), format = "%b %Y" ))
 
 
 ## sun position algorithm as the other broadband
 zenangle <- function(YYYY,min,doy){
     as.numeric(
         system(
-            paste("/home/athan/CM_21_GLB/BINARY/zenangle64 ", YYYY ,min, doy, " 40.634 -22.956" ),
+            paste("~/CM_21_GLB/BINARY/zenangle64 ", YYYY ,min, doy, " 40.634 -22.956" ),
             intern = T)
     )
 }
@@ -76,15 +74,15 @@ pzen <- function(YYYY, min = 1:1440, doy) {
 }
 
 
-
 BB <- opendata()
-
 
 yearstodo <- as.vector(BB |> filter(!is.na(DIR_wpsm)) |> select(year) |> unique() |> collect())
 
-yearstodo <- 2022
+# yearstodo <- 2022
 
 for (YYYY in yearstodo) {
+    pdf(file = paste0(DATOUT, "/", "CurrentDataPlots_", YYYY, ".pdf"))
+
     year_data <- data.table(
         BB |>
             filter(year == YYYY) |>
@@ -99,6 +97,9 @@ for (YYYY in yearstodo) {
                    ) |>
             collect()
     )
+
+    ## choose date range
+    year_data <- year_data[Date < MAX_DATE & Date > MIN_DATE]
 
     ## Recording limits
     year_data[, sig_lowlim := chp1_signal_lower_limit(Date)]
@@ -124,104 +125,55 @@ for (YYYY in yearstodo) {
     ## drop empty
     year_data <- year_data[!is.na(DIR_wpsm)]
 
-
-
-
-
-    stop()
-}
-
-
-
-
-
-
-stop()
-
-
-for (ayear in YEARS) {
-    pdf(file = paste0(DATOUT,"/","CurrentDataPlots_",ayear,".pdf") )
-
-    ## get data for a year
-    filein <- list.files(path         = DATIN,
-                         pattern      = paste0("LAP_CHP1_L1_", ayear, ".Rds"),
-                         full.names   = TRUE,
-                         include.dirs = FALSE )
-    stopifnot(length(filein) == 1)
-
-    data <- readRDS(filein)
-
-    # ## for others
-    # data <- data[!data$Async,]
-
-    ## subset data to export
-    data     <- data[ , wecare ]
-
-    ## complete all days
-    allmin <- seq( as.POSIXct(paste0(ayear, "-01-01 00:00:30 UTC")),
-                   as.POSIXct(paste0(ayear, "-12-31 24:00:00 UTC")), by = "min" )
-    allmin <- data.frame( Date30 = allmin )
-    data   <- merge(data, allmin ,  all.y = TRUE )
-
-    ## prepare data to export
-    data$Day          <- as.Date(data$Date30)
-    data$wattDIR[     is.na(data$wattDIR)     ] <- -9L
-    data$wattDIR_sds[ is.na(data$wattDIR_sds) ] <- -9L
-    data$SZA[         is.na(data$SZA)         ] <- -9L
-
-    yearfolder <- paste0(DATOUT,"/",ayear)
-    dir.create( yearfolder, showWarnings = F )
-
-
-    # ## export for others
-    # outdir <- "/home/athan/ZHOST/"
-    # data$Date30 <- data$Date30 - 30
-    # data$wattDIR[     data$wattDIR     <= -8     ] <- NA
-    # data$wattDIR_sds[ data$wattDIR_sds <= -8 ] <- NA
-    # data$SZA[         data$SZA  <= -8         ] <- NA
-    #
-    # data <- data[ !is.na(data$wattDIR), ]
-    # data <- data[ data$Date < "2022-02-28 00:00", ]
-    #
-    # names(data)[names(data) == "Date30"] <- "Date"
-    # data$Day <- NULL
-    # data <- data[!is.na(data$wattDIR),]
-    #
-    # myRtools::write_dat(object = data,
-    #                     file = sub( ".rds","", sub(DATIN, outdir, filein), ignore.case = T),
+    ## Simple export data for others -------------------------------------------
+    # myRtools::write_dat(object  = year_data,
+    #                     file    = paste0(OTHEREXPOR, "/", "CHP1_DIR_", YYYY),
     #                     contact = "<natsisphysicist@gmail.com>")
 
 
-    ## export each day
-    alldays <- sort(unique(data$Day))
+    ## make sure we have all minutes
+    allmin <- data.frame(
+        Date = seq(as.POSIXct(paste0(min(as.Date(year_data$Date)), " 00:00:30 UTC")),
+                   as.POSIXct(paste0(max(as.Date(year_data$Date)), " 23:59:30 UTC")),
+                   by = "min"))
+    year_data <- merge(year_data, allmin, all.y = TRUE)
+    # year_data[, .N, by = as.Date(Date)]
+
+    ## prepare data to export
+    year_data$Day <- as.Date(year_data$Date)
+    year_data$DIR_wpsm[   is.na(year_data$DIR_wpsm)   ] <- -9L
+    year_data$DIR_SD_wpsm[is.na(year_data$DIR_SD_wpsm)] <- -9L
+    year_data$SZA[        is.na(year_data$SZA)        ] <- -9L
+    ## create receiving folder
+    yearfolder <- paste0(DATOUT, "/", YYYY)
+    dir.create(yearfolder, showWarnings = F)
+
+    ## Export each day ---------------------------------------------------------
+    alldays <- sort(unique(year_data$Day))
 
     for (dd in alldays) {
-        dateD = as.Date(dd, origin = "1970-01-01")
-        cat(sprintf( "%s   \r", dateD ))
+        dateD <- as.Date(dd, origin = "1970-01-01")
+        cat(sprintf("%s   \r", dateD))
 
-        dayselec <- data$Day == dateD
+        dayselec <- year_data$Day == dateD
         if (sum(dayselec) != 1440 ) { stop("Day don't have 1440 minutes!!") }
 
         filename <- paste0(DATOUT, "/", strftime(dateD, format = "%Y/DIR%3j%y.DAT"))
-
-        oneday  <- data[ dayselec, ]
-
-        TIME_UT <- as.numeric((oneday$Date30 - as.POSIXct( dateD ) + 30) / 3600)
-
-        doy <- as.numeric(format( dateD, "%j" ))
+        oneday   <- year_data[dayselec, ]
+        TIME_UT  <- as.numeric((oneday$Date - as.POSIXct( dateD ) + 30) / 3600)
+        doy      <- yday(dateD)
 
         ## calculate zenith angle as other instruments
-        lapzen <- pzen(ayear, 1:1440, doy )
+        oneday$lapzen <- pzen(YYYY, 1:1440, doy )
 
-        output <- data.frame( TIME_UT = round(TIME_UT,            digits = 4 ),
-                              SZA     = round(lapzen,            digits = 2 ),
-                              # SZA     = round(oneday$SZA,         digits = 2 ),
-                              Wm2     = round(oneday$wattDIR,     digits = 3 ),
-                              st.dev  = round(oneday$wattDIR_sds, digits = 3 ) )
+        output <- data.frame(TIME_UT = round(TIME_UT,            digits = 4),
+                             SZA     = round(oneday$lapzen,      digits = 2),
+                             Wm2     = round(oneday$DIR_wpsm,    digits = 3),
+                             st.dev  = round(oneday$DIR_SD_wpsm, digits = 3))
 
         ## custom file header
-        write(" TIME_UT    SZA    [W.m-2]   st.dev\r\n",
-              file = filename)
+        cat(" TIME_UT    SZA    [W.m-2]   st.dev\r\n",
+            file = filename)
         ## format and write data
         write.table(format(output,
                            digits    = 3,
@@ -238,8 +190,7 @@ for (ayear in YEARS) {
         ## make sure has consistent line endings
         system(paste("unix2dos ", filename))
 
-
-
+stop()
         ## create a plot of the output data
         par(mar = c(3,3,1,1))
         par(mgp = c(1.5,.5,0))
@@ -258,22 +209,20 @@ for (ayear in YEARS) {
             title(main = paste(dateD), cex.main = .8 )
 
             points( output$TIME_UT, output$st.dev,
-                    pch = 18, cex = .4 , col = "blue")
+                    pch = 18, cex = .4, col = "blue")
 
             legend("topright", bty = "n",
-                   legend = c( "Direct Normal Irradiance",
-                               "Standard deviation")     ,
-                   lty = c(  1,  NA),
-                   pch = c( NA,  18),
+                   legend = c("Direct Normal Irradiance",
+                              "Standard deviation")     ,
+                   lty = c( 1, NA),
+                   pch = c(NA, 18),
                    col = c("green", "blue"),  cex = 0.8)
             legend("topleft", tag, bty = "n", cex = 0.8 )
 
-
-
             ## by sza
             plot( output$SZA, output$Wm2, "l",
-                  lwd  = 1.5 , col = "green",
-                  ylab = expression( Watt/m^2 ),
+                  lwd  = 1.5, col = "green",
+                  ylab = expression(Watt/m^2),
                   xlab = "SZA")
             title(main = paste(dateD), cex.main = .8 )
 
@@ -281,24 +230,22 @@ for (ayear in YEARS) {
                     pch = 18, cex = .4 , col = "blue")
 
             legend("topright", bty = "n",
-                   legend = c( "Direct Normal Irradiance",
-                               "Standard deviation")     ,
-                   lty = c(  1,  NA),
-                   pch = c( NA,  18),
+                   legend = c("Direct Normal Irradiance",
+                              "Standard deviation")     ,
+                   lty = c( 1, NA),
+                   pch = c(NA, 18),
                    col = c("green", "blue"),  cex = 0.8)
-            legend("topleft", tag, bty = "n", cex = 0.8 )
+            legend("topleft", tag, bty = "n", cex = 0.8)
 
 
         }## if data
         rm(output)
     }## alldays
     dev.off()
-}## all years
 
+    stop()
+}
 
-
-cat(paste("Before upload to sirena check line endings"))
-cat(paste("Run unix2dos"))
 
 
 #' **END**
