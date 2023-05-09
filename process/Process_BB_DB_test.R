de1ee92357e92715654ee9843aecfd6e4db2b70c
thanasisn <11601717+thanasisn@users.noreply.github.com>
2023-05-09 19:07:38 +0300

    Commit 2023-05-09 19:07

---
+++ process/Process_BB_DB_test.R
@@ -0,0 +1,28 @@
+#!/opt/R/4.2.3/bin/Rscript
+# /* Copyright (C) 2022-2023 Athanasios Natsis <natsisphysicist@gmail.com> */
+
+
+## __ Set environment  ---------------------------------------------------------
+rm(list = (ls()[ls() != ""]))
+Sys.setenv(TZ = "UTC")
+tic <- Sys.time()
+Script.Name <- "~/BBand_LAP/process/Process_BB_DB_test.R"
+
+
+library(rmarkdown)
+
+
+try({
+    render(input      = "~/BBand_LAP/process/Legacy_CM21_R10_export.R",
+           output_dir = "~/BBand_LAP/REPORTS/REPORTS")
+})
+
+try({
+    render(input      = "~/BBand_LAP/process/Legacy_CM21_R10_export.R",
+           output_dir = "~/BBand_LAP/REPORTS/REPORTS")
+})
+
+
+
+tac <- Sys.time()
+cat(sprintf("%s %s@%s %s %f mins\n\n",Sys.time(),Sys.info()["login"],Sys.info()["nodename"],Script.Name,difftime(tac,tic,units="mins")))
