868e391175a423dfc3a1f9ba20c5dbf7f6556e77
thanasisn <11601717+thanasisn@users.noreply.github.com>
2023-05-15 14:13:11 +0300

    Commit 2023-05-15 14:13

---
+++ tools/Read_steps_tracker.R
@@ -0,0 +1,34 @@
+#!/opt/R/4.2.3/bin/Rscript
+# /* Copyright (C) 2022-2023 Athanasios Natsis <natsisphysicist@gmail.com> */
+
+
+## __ Set environment  ---------------------------------------------------------
+rm(list = (ls()[ls() != ""]))
+Sys.setenv(TZ = "UTC")
+tic <- Sys.time()
+Script.Name <- "~/BBand_LAP/tools/Read_steps_tracker.R"
+
+
+source("~/BBand_LAP//DEFINITIONS.R")
+
+library(data.table)
+
+
+trSTP_DIR   <- "~/DATA_RAW/tracker_chp1/Tracker_STEP/"
+
+
+filesin <- list.files(trSTP_DIR,
+                      pattern = "sun_tracker_.*.stp",
+                      full.names = TRUE)
+
+gather <- data.table()
+for (af in filesin) {
+    gather <- rbind(gather, fread(af))
+
+}
+
+
+
+
+tac <- Sys.time()
+cat(sprintf("%s %s@%s %s %f mins\n\n",Sys.time(),Sys.info()["login"],Sys.info()["nodename"],Script.Name,difftime(tac,tic,units="mins")))
