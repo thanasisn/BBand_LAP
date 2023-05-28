d833ea29dd501980d661327d7be52bb97e84bc93
thanasisn <11601717+thanasisn@users.noreply.github.com>
2023-05-28 21:07:37 +0300

    Commit 2023-05-28 21:07

--- inspect_db/Inspect_BB_DB.R
+++ inspect_db/Inspect_BB_DB.R
@@ -59,28 +59,46 @@ try({
 
 
 
+try({
+    cat("\n\n 6. Inspect CM-21 signal CLEAN\n")
+    rmarkdown::render(input       = "~/BBand_LAP/inspect_db/Inspect_CM21INC_sig.R",
+                      params      = list(CLEAN = TRUE),
+                      output_file = "Inspect_CM21INC_sig_CLEAN.pdf",
+                      output_dir  = "~/BBand_LAP/REPORTS/REPORTS")
+})
+# try({
+#     cat("\n\n 7. Inspect CM-21 signal DIRTY\n")
+#     rmarkdown::render(input       = "~/BBand_LAP/inspect_db/Inspect_CM21INC_sig.R",
+#                       params      = list(CLEAN = FALSE),
+#                       output_file = "Inspect_CM21INC_sig_DIRTY.pdf",
+#                       output_dir  = "~/BBand_LAP/REPORTS/REPORTS")
+# })
+
+
+
+
 
 
 try({
-    cat("\n\n 6. Plot daily CHP-1 signals\n")
+    cat("\n\n 8. Plot daily CHP-1 signals\n")
     source("~/BBand_LAP/inspect_db/Plot_daily_CHP1_sig.R")
 })
 try({
-    cat("\n\n 7. Plot daily CM-21 signals\n")
+    cat("\n\n 9. Plot daily CM-21 signals\n")
     source("~/BBand_LAP/inspect_db/Plot_daily_CM21_sig.R")
 })
 
 
 
 try({
-    cat("\n\n 8. Inspect CHP-1 radiation\n")
+    cat("\n\n 10. Inspect CHP-1 radiation\n")
     rmarkdown::render(input       = "~/BBand_LAP/inspect_db/Inspect_CHP1_rad_temp.R",
                       # params      = list(CLEAN = FALSE),
                       output_dir  = "~/BBand_LAP/REPORTS/REPORTS")
 })
 
 try({
-    cat("\n\n 9. Inspect CM-21 radiation\n")
+    cat("\n\n 11. Inspect CM-21 radiation\n")
     rmarkdown::render(input       = "~/BBand_LAP/inspect_db/Inspect_CM21_rad.R",
                       # params      = list(CLEAN = FALSE),
                       output_dir  = "~/BBand_LAP/REPORTS/REPORTS")
@@ -90,18 +108,18 @@ try({
 
 
 try({
-    cat("\n\n 10. Plot daily radiation\n")
+    cat("\n\n 12. Plot daily radiation\n")
     source("~/BBand_LAP/inspect_db/Plot_daily_CHP1_L1.R")
 })
 try({
-    cat("\n\n 11. Plot daily radiation\n")
+    cat("\n\n 13. Plot daily radiation\n")
     source("~/BBand_LAP/inspect_db/Plot_daily_CM21_L1.R")
 })
 
 
 
 try({
-    cat("\n\n 12. Inspect CM-21 radiation\n")
+    cat("\n\n 14. Inspect CM-21 radiation\n")
     rmarkdown::render(input       = "~/BBand_LAP/inspect_db/Inspect_TOT_GLB.R",
                       output_dir  = "~/BBand_LAP/REPORTS/REPORTS")
 })
