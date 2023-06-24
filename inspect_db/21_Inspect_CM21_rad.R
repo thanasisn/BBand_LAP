c0bd8c738c7ee12a50b416cd8d1512f15fbd5a93
thanasisn <11601717+thanasisn@users.noreply.github.com>
2023-06-24 17:08:03 +0300

    Commit 2023-06-24 17:08

--- inspect_db/21_Inspect_CM21_rad.R
+++ inspect_db/21_Inspect_CM21_rad.R
@@ -66,7 +66,7 @@ knitr::opts_chunk$set(fig.pos   = '!h'    )
 ## __ Set environment  ---------------------------------------------------------
 Sys.setenv(TZ = "UTC")
 tic <- Sys.time()
-Script.Name <- "~/BBand_LAP/inspect_db/Inspect_CM21_rad.R"
+Script.Name <- "~/BBand_LAP/inspect_db/21_Inspect_CM21_rad.R"
 
 if (!interactive()) {
     pdf( file = paste0("~/BBand_LAP/REPORTS/RUNTIME/", basename(sub("\\.R$", ".pdf", Script.Name))))
