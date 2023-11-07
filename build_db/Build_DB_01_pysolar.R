7144ac3410cdac7721070dfa89d7021ea0f831b1
thanasisn <11601717+thanasisn@users.noreply.github.com>
2023-11-07 17:29:11 +0200

    Commit 2023-11-07 17:29

--- build_db/Build_DB_01_pysolar.R
+++ build_db/Build_DB_01_pysolar.R
@@ -78,7 +78,7 @@ if (file.exists(DB_META_fl)) {
     BB_meta$pysolar_basename         <- as.character(BB_meta$pysolar_basename)
     BB_meta$pysolar_mtime            <- as.POSIXct(BB_meta$pysolar_mtime)
     BB_meta$pysolar_parsed           <- as.POSIXct(BB_meta$pysolar_parsed)
-    BB_meta$daylength                <- as.numeric(NA)
+    BB_meta$daylength                <- as.integer(NA)
     ## For CM-21 meta data
     BB_meta$cm21_Daily_dark          <- as.numeric(NA)
     BB_meta$cm21_bad_data_flagged    <- as.POSIXct(NA)
