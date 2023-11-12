d761c1345bdf39b9242f6f891c9ea9db7f0e92e7
thanasisn <11601717+thanasisn@users.noreply.github.com>
2023-11-12 12:35:21 +0200

    Commit 2023-11-12 12:35

--- process/QCRad_LongShi_v9.R
+++ process/QCRad_LongShi_v9.R
@@ -710,7 +710,8 @@ for (af in filelist$names) {
 
     ##  Store data in the database  --------------------------------------------
     datapart <- as_tibble(datapart)
-    write_parquet(x = datapart, sink = af)
+    # write_parquet(x = datapart, sink = af)
+    writePARQUET(datapart, af)
     cat("Save: ", af, "\n\n")
 
     ##  Update meta data  ------------------------------------------------------
@@ -719,7 +720,8 @@ for (af in filelist$names) {
     update_meta[, (eval("QCv9_filters_md5")) := digest::digest(QS, algo = "md5")]
     ## insert new meta data
     BB_meta <- rows_update(BB_meta, update_meta, by = "day")
-    write_parquet(BB_meta, DB_META_fl)
+    # write_parquet(BB_meta, DB_META_fl)
+    writePARQUET(BB_meta, DB_META_fl)
 
     ##  Store filters parameters  ----------------------------------------------
     saveRDS(object = QS,
