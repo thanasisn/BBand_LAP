449cada1d7431af002367aca7f080f13f66a92da
thanasisn <11601717+thanasisn@users.noreply.github.com>
2024-09-26 22:13:33 +0300

    Commit 2024-09-26 22:13

--- build_duckdb/Build_DB_02_cm21.R
+++ build_duckdb/Build_DB_02_cm21.R
@@ -30,6 +30,7 @@ knitr::opts_chunk$set(fig.pos    = '!h'    )
 Sys.setenv(TZ = "UTC")
 tic <- Sys.time()
 Script.Name <- "~/BBand_LAP/build_duckdb/Build_DB_02_cm21.R"
+Script.ID   <- "02"
 # renv::load("~/BBand_LAP")
 
 if (!interactive()) {
@@ -107,6 +108,43 @@ update_table <- function(con,  new_data, table, matchvar) {
   dd1 <- data.table(names = colnames(new_data),
                     types = new_data |> head(1) |> collect() |> sapply(class))
 
+  if (!all(dd1$names %in% tt1$names)) {
+    ## get new variables
+    new_vars <- dd1[!names %in% tt1$names, ]
+    cat("New", new_vars$names)
+
+    for (i in 1:nrow(new_vars)) {
+
+      ## translate data types to duckdb
+      ctype <- switch(paste0(unlist(new_vars$types[i]), collapse = ""),
+                      POSIXctPOSIXt = "datetime",
+                      unlist(new_vars$types[i]))
+
+      cat("\nNEW VAR:", paste(new_vars[i, ]), "\n")
+
+      ## create new columns with a query
+      qq <- paste("ALTER TABLE", table,
+                  "ADD COLUMN",  new_vars$names[i], ctype, "DEFAULT null")
+      dbSendQuery(con, qq)
+    }
+  }
+
+  rows_update(x = tbl(con, table),
+              y = new_data,
+              by   = matchvar,
+              unmatched = "ignore",
+              in_place = TRUE,
+              copy = TRUE)
+}
+
+
+
+insert_table <- function(con,  new_data, table, matchvar) {
+  ## detect data types
+  tt1 <- data.table(names = colnames(tbl(con, table)),
+                    types = tbl(con, table) |> head(1) |> collect() |> sapply(class))
+  dd1 <- data.table(names = colnames(new_data),
+                    types = new_data |> head(1) |> collect() |> sapply(class))
 
   if (!all(dd1$names %in% tt1$names)) {
     ## get new variables
@@ -129,75 +167,76 @@ update_table <- function(con,  new_data, table, matchvar) {
     }
   }
 
-  rows_update(tbl(con, table),
-              new_data,
-              by   = matchvar,
-              unmatched = "ignore",
+  rows_insert(x  = tbl(con, table),
+              y  = new_data,
+              by = matchvar,
+              conflict = "ignore",
               in_place = TRUE,
-              copy = TRUE)
+              copy     = TRUE)
 }
 
-inp_filelist <- data.table::first(inp_filelist, n = 20)
-
-for (ll in 1:nrow(inp_filelist)) {
-  ff <- inp_filelist[ll, ]
-
-  cat("02 : ", basename(ff$fullname), ff$Day, ll,"/",nrow(inp_filelist),"\n")
-
-  ## prepare input file data
-  suppressWarnings(rm(D_minutes))
-  D_minutes <- seq(from       = as.POSIXct(paste(as_date(ff$Day), "00:00:30"), tz = ""),
-                   length.out = 1440,
-                   by         = "min")
-
-  lap    <- fread(ff$fullname, na.strings = "-9")
-  lap$V1 <- as.numeric(lap$V1)
-  lap$V2 <- as.numeric(lap$V2)
-  stopifnot(is.numeric(lap$V1))
-  stopifnot(is.numeric(lap$V2))
-  stopifnot(dim(lap)[1] == 1440)
-  lap[V1 < -8, V1 := NA]
-  lap[V2 < -8, V2 := NA]
-
-  day_data <- data.table(Date        = as.POSIXct(D_minutes),      # Date of the data point
-                         CM21_sig    = lap$V1,         # Raw value for CM21
-                         CM21_sig_sd = lap$V2)         # Raw SD value for CM21
-  day_data$Epoch <- as.integer(day_data$Date)
-  day_data$Date <- NULL
 
 
-  ## meta data for file
-  file_meta <- data.table(Day             = ff$Day,
-                          cm21_basename   = basename(ff$fullname),
-                          cm21_mtime      = file.mtime(ff$fullname),
-                          cm21_parsed     = Sys.time(),
-                          cm21_md5sum     = as.vector(md5sum(ff$fullname)))
+if (nrow(inp_filelist) > 0) {
+  for (ll in 1:nrow(inp_filelist)) {
+    ff <- inp_filelist[ll, ]
 
-  ## Add data
-  update_table(con      = con,
-               new_data = day_data,
-               table    = "LAP",
-               matchvar = "Epoch")
+    cat(Script.ID, ": ", basename(ff$fullname), ff$Day, ll,"/",nrow(inp_filelist),"\n")
 
+    ## prepare input file data
+    suppressWarnings(rm(D_minutes))
+    D_minutes <- seq(from       = as.POSIXct(paste(as_date(ff$Day), "00:00:30"), tz = ""),
+                     length.out = 1440,
+                     by         = "min")
 
-  ## Add metadata
-  if (!dbExistsTable(con, "META")) {
-    ## Create new table
-    cat("\n Initialize table 'META' \n\n")
-    dbWriteTable(con, "META", file_meta)
-    db_create_index(con, "META", columns = "Day", unique = TRUE)
-  } else {
-    ## Append new data
-    cat("\n Add data to 'META' \n\n")
+    lap    <- fread(ff$fullname, na.strings = "-9")
+    lap$V1 <- as.numeric(lap$V1)
+    lap$V2 <- as.numeric(lap$V2)
+    stopifnot(is.numeric(lap$V1))
+    stopifnot(is.numeric(lap$V2))
+    stopifnot(dim(lap)[1] == 1440)
+    lap[V1 < -8, V1 := NA]
+    lap[V2 < -8, V2 := NA]
 
+    day_data <- data.table(Date        = as.POSIXct(D_minutes),      # Date of the data point
+                           CM21_sig    = lap$V1,         # Raw value for CM21
+                           CM21_sig_sd = lap$V2)         # Raw SD value for CM21
+    day_data$Epoch <- as.integer(day_data$Date)
+    day_data$Date <- NULL
+
+    ## meta data for file
+    file_meta <- data.table(Day             = ff$Day,
+                            cm21_basename   = basename(ff$fullname),
+                            cm21_mtime      = file.mtime(ff$fullname),
+                            cm21_parsed     = Sys.time(),
+                            cm21_md5sum     = as.vector(md5sum(ff$fullname)))
+
+    ## Add data
     update_table(con      = con,
-                 new_data = file_meta,
-                 table    = "META",
-                 matchvar = "Day")
+                 new_data = day_data,
+                 table    = "LAP",
+                 matchvar = "Epoch")
+
+    ## Add metadata
+    if (!dbExistsTable(con, "META")) {
+      ## Create new table
+      cat("\n Initialize table 'META' \n\n")
+      dbWriteTable(con, "META", file_meta)
+      db_create_index(con, "META", columns = "Day", unique = TRUE)
+    } else {
+      ## Append new data
+      insert_table(con      = con,
+                   new_data = file_meta,
+                   table    = "META",
+                   matchvar = "Day")
+    }
+
+
   }
-
-
+} else {
+  cat(Script.ID, ": ", "No new files to add\n\n")
 }
+
 
 dbDisconnect(con)
 
@@ -208,51 +247,22 @@ stop()
 
 
 
-
-
-
-
-# unmatched = "ignore",
-# in_place = T)
-
-
+con   <- dbConnect(duckdb(dbdir = DB_DUCK))
 
 
 tbl(con, "LAP") |> colnames()
+tbl(con, "META") |> colnames()
 
 
-tbl(con, "LAP") |> filter(!is.na(CM21_sig)) |> glimpse()
-tbl(con, "LAP") |> filter(!is.na(CM21_sig)) |> tally()
+tbl(con, "LAP")  |> filter(!is.na(CM21_sig)) |> glimpse()
 
-stop()
-##  Import CM-21 files  --------------------------------------------------------
-for (YYYY in unique(year(inp_filelist$day))) {
-    subyear <- inp_filelist[year(day) == YYYY]
-    ## months to do
-    for (mm in subyear[, unique(month(day))]) {
-        submonth <- subyear[month(day) == mm]
+tbl(con, "LAP")  |> filter(!is.na(CM21_sig)) |> tally()
+tbl(con, "LAP")  |> filter(!is.na(CM21_sig)) |> distinct(Day) |> tally()
 
-
-        ##  read this month set files
-        gathermeta <- data.table()
-        for (ad in submonth$day) {
-            ss <- submonth[day == ad]
-
-            suppressWarnings(rm(D_minutes))
-            D_minutes <- seq(from       = as.POSIXct(paste(as_date(ad), "00:00:30 UTC")),
-                             length.out = 1440,
-                             by         = "min" )
+tbl(con, "META") |> tally()
 
 
 
-            # gather <- rows_patch(gather, day_data, by = "Date")
-            # gather     <- rows_update(gather, day_data, by = "Date")
-            gather     <- rows_upsert(gather, day_data, by = "Date")
-            gathermeta <- rbind(gathermeta, file_meta)
-        }
-
-    }
-}
 
 
 
