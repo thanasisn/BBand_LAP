89488180d57f023c5ddf8266c33a6b06111b5d26
thanasisn <11601717+thanasisn@users.noreply.github.com>
2024-10-04 13:07:51 +0300

    Commit 2024-10-04 13:07

--- build_duckdb/Build_DB_42_cm21_dark_radiat.R
+++ build_duckdb/Build_DB_42_cm21_dark_radiat.R
@@ -29,10 +29,12 @@ knitr::opts_chunk$set(fig.pos   = '!h'    )
 
 
 ## __ Set environment  ---------------------------------------------------------
+closeAllConnections()
 Sys.setenv(TZ = "UTC")
 tic <- Sys.time()
 Script.Name <- "~/BBand_LAP/build_duckdb/Build_DB_42_cm21_dark_radiat.R"
-# renv::load("~/BBand_LAP")
+Script.ID   <- "42"
+
 
 if (!interactive()) {
     pdf( file = paste0("~/BBand_LAP/REPORTS/RUNTIME/", basename(sub("\\.R$", ".pdf", Script.Name))))
@@ -60,27 +62,178 @@ library(duckdb,     warn.conflicts = FALSE, quietly = TRUE)
 con   <- dbConnect(duckdb(dbdir = DB_DUCK))
 
 
-##
+##  Create a dummy column if not exist
 make_new_column(con, "META", "cm21_dark_flag", "character")
+
+
+
+
+
+## Create a dark construct!  ---------------------------------------------------
+
+## create construct if are available data
+vddays <- tbl(con, "META") |> filter(!is.na(cm21_dark_flag)) |> tally() |> pull()
+if (vddays > 100) {
+  test <- tbl(con, "META") |>
+    select(Day,
+           cm21_dark_flag,
+           cm21_dark_Eve_med,
+           cm21_dark_Mor_med,
+           cm21_Daily_dark) |>
+    filter(!is.na(cm21_Daily_dark))  |>
+    collect() |> data.table()
+  ## will use mean Daily dark
+  cm21EVEdark   <- approxfun(test$Day, test$cm21_dark_Eve_med)
+  cm21MORdark   <- approxfun(test$Day, test$cm21_dark_Mor_med)
+  cm21DAILYdark <- approxfun(test$Day, test$cm21_Daily_dark  )
+
+  ## get dark missing days
+  missingdays <- tbl(con, "META") |>
+    filter(!is.na(cm21_basename)) |>
+    filter(cm21_dark_flag %in% c("MISSING", "CONSTRUCTED")) |>
+    select(Day) |> collect() |> data.table()
+  ## should we use missing only?
+  ## Create missing dark
+  construct <- data.table(
+    Date = missingdays,
+    DARK = cm21DAILYdark(missingdays)
+  )
+  # plot(test$day, test$cm21_dark_Eve_med)
+  # plot(test$day, test$cm21_dark_Mor_med)
+  plot(test$Day, test$cm21_Daily_dark,
+       main = "Constructed Dark values for CM-21")
+  points(construct$Date, construct$DARK, col = "red")
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 ## days to loop
 dayslist <- tbl(con, "META") |> filter(is.na(cm21_dark_flag) & !is.na(cm21_basename)) |> select(Day) |> pull()
 
 ## go find dark and radiation
+cc <- 0
 for (ad in dayslist) {
   ad <- as.Date(ad, origin = origin)
-  tbl(con, "LAP") |>
+  cc <- cc + 1
+  cat(Script.ID, ":", paste(ad), cc, "/",length(dayslist), "\n")
+
+  ## use only valid data for dark calculation
+  daydata <-
+    tbl(con, "LAP")                   |>
     filter(Day == ad)                 |>  ## this day only
     filter(!is.na(CM21_sig))          |>  ## valid measurements
     filter(is.na(cm21_bad_data_flag)) |>  ## not bad data
-    collect() |> data.table()
+    filter(cm21_sig_limit_flag == 0)  |>  ## acceptable values range
+    collect() |>
+    data.table()
+
+  ## Ignore bad and missing data
+  if (nrow(daydata) == 0) {
+    cat("     No usefull CM-21 data in this file\n\n")
+    next()
+  }
+  if (any(is.na(daydata$Elevat))) {
+    cat("     The day is not initialized:", paste(ad),"\n")
+    next()
+  }
 
 
-  ## use only valid data for dark
-  data_use <- datapart[is.na(cm21_bad_data_flag) &
-                         !is.na(CM21_sig) &
-                         is.na(CM21_sig_wo_dark)]
+
+  ## __ Compute dark values for day  -------------------------------------
+  dark_day <- dark_calculations_2(
+    dates      = daydata$Date,
+    values     = daydata$CM21_sig,
+    elevatio   = daydata$Eleva,
+    nightlimit = DARK_ELEV,
+    dstretch   = DSTRETCH
+  )
+
+
+
+  ## __ Resolve problematic dark calculations ----------------------------
+
+  ## no data to use
+  if (all(is.na(daydata$CM21_sig))) {
+    dark_flag              <- "NO SIGNAL DATA"
+    todays_dark_correction <- as.numeric(NA)
+    missingdark            <- as.numeric(NA)
+    ## data to compute dark are missing
+  } else if ( !((!is.na(dark_day$dark_Mor_med) & dark_day$dark_Mor_cnt >= DCOUNTLIM) |
+                (!is.na(dark_day$dark_Eve_med) & dark_day$dark_Eve_cnt >= DCOUNTLIM))) {
+    todays_dark_correction <- as.numeric(NA)
+    dark_flag              <- "MISSING"
+    missingdark            <- as.numeric(NA)
+
+    ## get dark from pre-computed file
+    if (exists("construct")) {
+      ## can not find date
+      if (!aday %in% construct$Date) {
+        todays_dark_correction <- as.numeric(NA)
+        dark_flag              <- "MISSING"
+        missingdark            <- as.numeric(NA)
+      } else {
+        ## get data from recomputed dark database
+        todays_dark_correction <- construct[Date == aday, DARK]
+        dark_flag              <- "CONSTRUCTED"
+      }
+    } else {
+      cat("Need to constract dark:", format(as.Date(aday)),"\n")
+    }
+  } else {
+    ## __ Dark Correction function for non missing  --------------------
+    dark_generator <- dark_function_2(dark_day    = dark_day,
+                                      DCOUNTLIM   = DCOUNTLIM,
+                                      type        = "median",
+                                      missingdark = missingdark)
+    ## Create dark signal for every minute
+    todays_dark_correction <- dark_generator(daydata$Date)
+    dark_flag              <- "COMPUTED"
+  }
+
+  ## __ Apply dark correction for the day  -------------------------------
+  daydata[, CM21_sig_wo_dark := CM21_sig - todays_dark_correction]
+
+  ## __ Convert signal to radiation --------------------------------------
+  daydata[, GLB_wpsm    := CM21_sig_wo_dark * cm21factor(Date)]
+  daydata[, GLB_SD_wpsm := CM21_sig_sd      * cm21factor(Date)]
+
+
+  ## __ Day stats --------------------------------------------------------
+  names(dark_day) <- paste0("cm21_", names(dark_day))
+  meta_day <- data.frame(Day                = as.Date(ad),
+                         cm21_Daily_dark    = mean(todays_dark_correction, na.rm = T),
+                         cm21_dark_flag     = dark_flag,
+                         dark_day,
+                         cm21_dark_computed = Sys.time()
+  )
+
+  ## __  Store data in the database  -------------------------------------------
+  update_table(con      = con,
+               new_data = daydata,
+               table    = "LAP",
+               matchvar = "Date")
+  update_table(con      = con,
+               new_data = meta_day,
+               table    = "META",
+               matchvar = "Day")
+
 
 }
 
@@ -93,8 +246,8 @@ for (ad in dayslist) {
 tbl(con, "META") |> glimpse()
 tbl(con, "LAP")  |> glimpse()
 
-tbl(con, "LAP") |> filter(!is.na(cm21_bad_data_flag))
-tbl(con, "LAP") |> filter(!is.na(cm21_bad_data_flag))
+tbl(con, "LAP") |> filter(!is.na(cm21_bad_data_flag)) |> glimpse()
+tbl(con, "LAP") |> filter(cm21_sig_limit_flag == 0)
 
 stop()
 
@@ -106,154 +259,21 @@ stop()
 ##  Dark calculations on dataset  ----------------------------------------------
 
 
-## select what dataset files to touch
-filelist <- filelist[todosets, on = .(flmonth = month, flyear = year)]
 
 
 
-## Create a dark construct!  ---------------------------------------------------
-
-## create construct if are available data
-if (BB_meta[!is.na(cm21_dark_flag), .N] > 100) {
-    test <- BB_meta[, .(day, cm21_dark_flag, cm21_dark_Eve_med, cm21_dark_Mor_med, cm21_Daily_dark) ]
-    ## will use mean Daily dark
-    cm21EVEdark   <- approxfun(test$day, test$cm21_dark_Eve_med)
-    cm21MORdark   <- approxfun(test$day, test$cm21_dark_Mor_med)
-    cm21DAILYdark <- approxfun(test$day, test$cm21_Daily_dark  )
-    ## get dark missing days
-    missingdays <- BB_meta[!is.na(cm21_basename) &
-                           cm21_dark_flag %in% c("MISSING", "CONSTRUCTED"), day]
-    ## should we use missing only?
-
-    ## Create missing dark
-    construct <- data.table(
-        Date = missingdays,
-        DARK = cm21DAILYdark(missingdays)
-    )
-    # plot(test$day, test$cm21_dark_Eve_med)
-    # plot(test$day, test$cm21_dark_Mor_med)
-    plot(test$day, test$cm21_Daily_dark,
-         main = "Constructed Dark values for CM-21")
-    points(construct$Date, construct$DARK, col = "red")
-}
-
-
-rm(todosets, dd, test)
 
 
 
-## loop data base files computing black for CM-21
-for (af in filelist$names) {
-    datapart <- data.table(read_parquet(af))
-    datapart[, month := as.integer(month(Date))]
-    datapart[, year  := as.integer(year(Date)) ]
 
-    ## use only valid data for dark
-    data_use <- datapart[is.na(cm21_bad_data_flag) &
-                         !is.na(CM21_sig) &
-                         is.na(CM21_sig_wo_dark)]
 
-    cat("42 Load: ", af, "\n")
 
-    ## Ignore bad and missing data
-    if (nrow(data_use) == 0) {
-        cat("\nNo usefull CM-21 data in this file\n\n")
-        next()
-    }
 
-    ## loop days
-    for (aday in unique(as.Date(data_use$Date))) {
-        daydata <- data_use[ as.Date(Date) == aday ]
 
-        if (any(is.na(daydata$Elevat))) {
-            cat("The day is not initialized:", format(as.Date(aday, origin = "1970-01-01")),"\n")
-            next()
-        }
 
-        ## __ Compute dark values for day  -------------------------------------
-        dark_day <- dark_calculations_2(
-            dates      = daydata$Date,
-            values     = daydata$CM21_sig,
-            elevatio   = daydata$Eleva,
-            nightlimit = DARK_ELEV,
-            dstretch   = DSTRETCH
-        )
 
-        ## __ Resolve problematic dark calculations ----------------------------
 
-        ## no data to use
-        if (all(is.na(daydata$CM21_sig))) {
-            dark_flag              <- "NO SIGNAL DATA"
-            todays_dark_correction <- as.numeric(NA)
-            missingdark            <- as.numeric(NA)
-        ## data to compute dark are missing
-        } else if ( !((!is.na(dark_day$dark_Mor_med) & dark_day$dark_Mor_cnt >= DCOUNTLIM) |
-                      (!is.na(dark_day$dark_Eve_med) & dark_day$dark_Eve_cnt >= DCOUNTLIM))) {
-            todays_dark_correction <- as.numeric(NA)
-            dark_flag              <- "MISSING"
-            missingdark            <- as.numeric(NA)
 
-            ## get dark from pre-computed file
-            if (exists("construct")) {
-                ## can not find date
-                if (!aday %in% construct$Date) {
-                    todays_dark_correction <- as.numeric(NA)
-                    dark_flag              <- "MISSING"
-                    missingdark            <- as.numeric(NA)
-                } else {
-                    ## get data from recomputed dark database
-                    todays_dark_correction <- construct[Date == aday, DARK]
-                    dark_flag              <- "CONSTRUCTED"
-                }
-            } else {
-                cat("Need to constract dark:", format(as.Date(aday)),"\n")
-            }
-        } else {
-            ## __ Dark Correction function for non missing  --------------------
-            dark_generator <- dark_function_2(dark_day    = dark_day,
-                                              DCOUNTLIM   = DCOUNTLIM,
-                                              type        = "median",
-                                              missingdark = missingdark)
-            ## Create dark signal for every minute
-            todays_dark_correction <- dark_generator(daydata$Date)
-            dark_flag              <- "COMPUTED"
-        }
-
-        ## __ Apply dark correction for the day  -------------------------------
-        daydata[, CM21_sig_wo_dark := CM21_sig - todays_dark_correction]
-
-        ## __ Convert signal to radiation --------------------------------------
-        daydata[, GLB_wpsm    := CM21_sig_wo_dark * cm21factor(Date)]
-        daydata[, GLB_SD_wpsm := CM21_sig_sd      * cm21factor(Date)]
-
-        # ## sanity check!
-        # test <- daydata[GLB_wpsm > 2000 | GLB_wpsm < 100]
-        # if (nrow(test) > 0) {
-        #     cat(paste(format(unique(as.Date(test$Date))), collapse = " "),"\n")
-        #     stop("Sanity check failed: Global out of range!!")
-        # }
-
-        ## __ Day stats --------------------------------------------------------
-        names(dark_day) <- paste0("cm21_", names(dark_day))
-        meta_day <- data.frame(day                = as.Date(aday),
-                               cm21_Daily_dark    = mean(todays_dark_correction, na.rm = T),
-                               cm21_dark_flag     = dark_flag,
-                               dark_day,
-                               cm21_dark_computed = Sys.time()
-        )
-
-        ## import new data
-        BB_meta  <- rows_update(BB_meta, meta_day, by = "day")
-        ## update initial data with valid only
-        datapart <- rows_update(datapart, daydata, by = "Date")
-        rm(daydata, meta_day, dark_day)
-    }
-
-    ## store actual data
-    write_parquet(x = datapart, sink = af)
-    write_parquet(BB_meta, DB_META_fl)
-    cat("42 Save: ", af, "\n\n")
-}
 
 
 print(
