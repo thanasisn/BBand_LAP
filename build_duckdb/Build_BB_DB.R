ebeec926cac4a0757044eb8bb39afa0ff9300305
thanasisn <11601717+thanasisn@users.noreply.github.com>
2024-10-05 01:08:36 +0300

    Commit 2024-10-05 01:08

--- build_duckdb/Build_BB_DB.R
+++ build_duckdb/Build_BB_DB.R
@@ -23,7 +23,7 @@ source("~/BBand_LAP/build_duckdb/Build_DB_02_cm21.R"           )
 source("~/BBand_LAP/build_duckdb/Build_DB_03_chp1.R"           )
 source("~/BBand_LAP/build_duckdb/Build_DB_04_chp1_SNC.R"       )
 source("~/BBand_LAP/build_duckdb/Build_DB_05_chp1_TMP.R"       )
-# source("~/BBand_LAP/build_duckdb/Build_DB_06_cm21inclined.R"    )
+source("~/BBand_LAP/build_duckdb/Build_DB_06_cm21inclined.R"   )
 
 
 # source("~/BBand_LAP/build_duckdb/Build_DB_16_cm21_TOT.R"        )
