74c8c97950a631edf1a7f664e12a301052775180
thanasisn <11601717+thanasisn@users.noreply.github.com>
2024-10-05 07:08:53 +0300

    Commit 2024-10-05 07:08

--- build_duckdb/Build_DB_16_cm21_TOT.R
+++ build_duckdb/Build_DB_16_cm21_TOT.R
@@ -46,6 +46,9 @@ require(duckdb,     warn.conflicts = FALSE, quietly = TRUE)
 
 cat("\n Import  CM-21  TOT  data\n\n")
 
+##  Open dataset  --------------------------------------------------------------
+con   <- dbConnect(duckdb(dbdir = DB_DUCK))
+
 ##  Get TOT CM-21 files  ------------------------------------------------------
 inp_filelist <- list.files(path        = SIRENA_TOT,
                            recursive   = TRUE,
@@ -60,51 +63,125 @@ inp_filelist <- data.table(fullname = inp_filelist)
 inp_filelist[, tot_cm21_basename := basename(fullname)]
 stopifnot( all(duplicated(sub("\\..*", "", inp_filelist$tot_cm21_basename))) == FALSE )
 
-inp_filelist$day <- as.Date(parse_date_time(
+inp_filelist$Day <- as.Date(parse_date_time(
     sub("\\.dat", "", sub("TOT", "", inp_filelist$tot_cm21_basename, ignore.case = T), ignore.case = T),
     "jy"))
-setorder(inp_filelist, day)
+setorder(inp_filelist, Day)
 cat("\n**Found:",paste(nrow(inp_filelist), "TOT CM-21 files**\n"))
 
+## check files have unique days
+stopifnot(!any(duplicated(inp_filelist$Day)))
 
-## only new files in the date range
-inp_filelist <- inp_filelist[!inp_filelist$tot_cm21_basename %in% BB_meta$tot_cm21_basename]
-inp_filelist <- inp_filelist[inp_filelist$day %in% BB_meta$day]
+## keep only files which correspond to existing dates
+inp_filelist <- right_join(inp_filelist,
+                           tbl(con, "LAP") |>
+                             select(Day)   |>
+                             distinct()    |>
+                             collect(),
+                           by = "Day") |>
+  filter(!is.na(tot_cm21_basename))
+
+## add only files not in the metadata
+if (dbExistsTable(con, "META") &
+    any(dbListFields(con, "META") %in% "tot_cm21_basename")) {
+  inp_filelist <- anti_join(inp_filelist,
+                            tbl(con, "META") |>
+                              filter(!is.na(tot_cm21_basename)) |>
+                              select(Day) |>
+                              collect(),
+                            by = "Day") |>
+    filter(!is.na(tot_cm21_basename))
+}
 
 cat("\n**Parse:",paste(nrow(inp_filelist), "TOT CM-21 files**\n\n"))
 
-
-stop()
 ##  Import TOT CM-21 files  ----------------------------------------------------
+if (nrow(inp_filelist) > 0) {
+  for (ll in 1:nrow(inp_filelist)) {
+    ff <- inp_filelist[ll, ]
+
+    cat(Script.ID, ": ",
+        basename(ff$fullname),
+        paste(ff$Day),
+        ll,"/",nrow(inp_filelist), "\n")
+
+
+
+
+
+    stop()
+
+    ## prepare input file data
+    suppressWarnings(rm(D_minutes))
+    D_minutes <- seq(from       = as.POSIXct(paste(as_date(ff$Day), "00:00:30"), tz = ""),
+                     length.out = 1440,
+                     by         = "min")
+
+    lap    <- fread(ff$fullname, na.strings = "-9")
+    lap$V1 <- as.numeric(lap$V1)
+    lap$V2 <- as.numeric(lap$V2)
+    stopifnot(is.numeric(lap$V1))
+    stopifnot(is.numeric(lap$V2))
+    stopifnot(dim(lap)[1] == 1440)
+    lap[V1 < -8, V1 := NA]
+    lap[V2 < -8, V2 := NA]
+
+
+
+
+    day_data <- data.table(Date        = D_minutes,      # Date of the data point
+                           CM21_sig    = lap$V1,         # Raw value for CM21
+                           CM21_sig_sd = lap$V2)         # Raw SD value for CM21
+
+    ## try to fix dates
+    day_data[, Date := round_date(Date, unit = "second")]
+
+
+
+    ## meta data for file
+    file_meta <- data.table(Day           = ff$Day,
+                            cm21_basename = basename(ff$fullname),
+                            cm21_mtime    = file.mtime(ff$fullname),
+                            cm21_parsed   = Sys.time(),
+                            cm21_md5sum   = as.vector(md5sum(ff$fullname)))
+
+    ## Add data and metadata
+    {
+      update_table(con      = con,
+                   new_data = day_data,
+                   table    = "LAP",
+                   matchvar = "Date")
+
+      ## Add metadata
+      if (!dbExistsTable(con, "META")) {
+        ## Create new table
+        cat("\n Initialize table 'META' \n\n")
+        dbWriteTable(con, "META", file_meta)
+      }
+
+      ## Append new data
+      update_table(con      = con,
+                   new_data = file_meta,
+                   table    = "META",
+                   matchvar = "Day")
+
+    }
+  }
+} else {
+  cat(Script.ID, ": ", "No new files to add\n\n")
+}
+
+
+
+
+
+
+
+
 for (YYYY in unique(year(inp_filelist$day))) {
     subyear <- inp_filelist[year(day) == YYYY]
     ## months to do
     for (mm in subyear[, unique(month(day))]) {
-        submonth <- subyear[month(day) == mm]
-        ## export file name and hive dir
-        filedir <- paste0(DB_DIR, "/", YYYY, "/", mm, "/")
-        dir.create(filedir, recursive = TRUE, showWarnings = FALSE)
-        partfile <- paste0(filedir, "/part-0.parquet")
-        ## init data collector
-        if (file.exists(partfile)) {
-            cat("16 Load: ", partfile, "\n")
-            gather <- read_parquet(partfile)
-            var <- "year"
-            if (!any(names(gather) == var)) {
-                gather[[var]] <- NA
-                gather[[var]] <- as.integer(gather[[var]])
-            }
-            var <- "month"
-            if (!any(names(gather) == var)) {
-                gather[[var]] <- NA
-                gather[[var]] <- as.integer(gather[[var]])
-            }
-        } else {
-            cat("Skipping new rows data inport", partfile, "\n")
-            next()
-            ## This can work, but there is no need for it.
-            ## Sun script should initialize the DB rows.
-        }
 
         ##  read this month set files
         gathermeta <- data.table()
@@ -144,8 +221,6 @@ for (YYYY in unique(year(inp_filelist$day))) {
                                     tot_cm21_basename   = basename(ss$fullname),
                                     tot_cm21_mtime      = file.mtime(ss$fullname),
                                     tot_cm21_parsed     = Sys.time(),
-                                    tot_cm21_glb_NAs    = sum(is.na(temp$tot_glb)),
-                                    tot_cm21_glb_sd_NAs = sum(is.na(temp$tot_glb_sd)),
                                     tot_cm21_md5sum     = as.vector(md5sum(ss$fullname)))
 
             # gather <- rows_patch(gather, day_data, by = "Date")
@@ -165,15 +240,8 @@ for (YYYY in unique(year(inp_filelist$day))) {
 
         setorder(gather, Date)
 
-        ## store this month / set data
-        write_parquet(gather,  partfile)
-        write_parquet(BB_meta, DB_META_fl)
-        cat("16 Save: ", partfile, "\n")
-        rm(gather, gathermeta, submonth)
     }
-    rm(subyear)
 }
-rm(inp_filelist)
 
 
 
