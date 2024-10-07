79db0770c09926e00976229e730975d1eeda0c3b
thanasisn <11601717+thanasisn@users.noreply.github.com>
2024-10-07 06:13:40 +0300

    Commit 2024-10-07 06:13

--- build_duckdb/Build_DB_01_initialize.R
+++ build_duckdb/Build_DB_01_initialize.R
@@ -58,9 +58,6 @@ cat("\n Initialize DB and/or import Sun data\n\n")
 con <- dbConnect(duckdb(dbdir = DB_DUCK))
 sun <- dbConnect(duckdb(dbdir = DB_LAP, read_only = TRUE))
 
-
-## TEST
-con <- dbConnect(duckdb(dbdir = "~/ZHOST/test.duckdb"))
 ##  Get Astropy data  ----------------------------------------------------------
 SUN <- tbl(sun, "params") |>
   filter(!is.na(AsPy_Elevation) & Date >= DB_start_date) |>
@@ -68,16 +65,6 @@ SUN <- tbl(sun, "params") |>
   rename(Azimuth          = "AsPy_Azimuth")              |>
   rename(Sun_Dist_Astropy = "AsPy_Dist")                 |>
   rename(Elevat           = "AsPy_Elevation")
-
-## select what is missing to add
-if (dbExistsTable(con, "LAP")) {
-  SUN <- anti_join(SUN,
-                   tbl(con, "LAP")        |>
-                     select(Date)         |>
-                     filter(!is.na(Date)) |>
-                     collect(),
-                   by = "Date")
-}
 
 ##  Create some useful variables
 SUN <- SUN |> mutate(
@@ -89,46 +76,52 @@ SUN <- SUN |> mutate(
   preNoon = case_when(
     Azimuth <= 180 ~ TRUE,
     Azimuth >  180 ~ FALSE
-  ) |>
-    arrange(Date)
+  )
 )
-
-
-
-##  Info display
-cat(paste(
-  Script.ID,
-  ":",
-  SUN |> distinct(Day) |> tally() |> pull(),
-  "New days"),
-  sep = "\n")
-
-SUN |> head()
-
-stop()
 
 ##  Add data  ------------------------------------------------------------------
 if (!dbExistsTable(con, "LAP")) {
   ## Create new table
   cat("\n Initialize table 'LAP' \n\n")
-  dbWriteTable(con, "LAP", SUN)
+  yto <- SUN |> summarise(min(year, na.rm = T)) |> pull()
+  ADD <- SUN |> filter(year == yto) |> collect() |> data.table() |> arrange(Date)
+  cat(paste(Script.ID, ":",
+            ADD |> distinct(Day) |> tally() |> pull(),
+            "New days",
+            paste(ADD |> reframe(range(Date)) |> pull(), collapse = " -- ")
+  ), sep = "\n")
+  dbWriteTable(con, "LAP", ADD)
   ## indexing will block drop of columns for now!!
   # db_create_index(con, "LAP", columns = "Date", unique = TRUE)
 } else {
   ## Append new data
-  cat("\n Add data to 'LAP' \n\n")
-  dbWriteTable(con, "LAP", SUN, append = TRUE)
+  cat("\n Add more data to 'LAP' \n\n")
+  SUN <- anti_join(SUN,
+                   tbl(con, "LAP") |> select(Date) |> filter(!is.na(Date)) |> collect(),
+                   by = "Date",
+                   copy = TRUE)
+
+  ytodo <- SUN |> select(year) |> distinct() |> pull()
+  for (yto in sort(ytodo)) {
+    ADD <- SUN |> filter(year == yto) |> collect() |> data.table() |> arrange(Date)
+    cat(paste(Script.ID, ":",
+              ADD |> distinct(Day) |> tally() |> pull(),
+              "New days",
+              paste(ADD |> reframe(range(Date)) |> pull(), collapse = " -- ")
+    ), sep = "\n")
+    dbWriteTable(con, "LAP", ADD, append = TRUE)
+  }
 }
 
 ##  Create all corresponding metadata days
 if (!dbExistsTable(con, "META")) {
-
+  cat("\n Initialize table 'META' \n\n")
   ## FIXME should be done in pure SQL
   init <- tbl(con, "LAP") |> select(Day) |> distinct() |> collect() |> data.table()
-
   dbWriteTable(con, "META", init)
 } else {
   ##  Add new dates
+  cat("\n Extend table 'META' \n\n")
   rows_insert(
     tbl(con, "META"),
     tbl(con, "LAP") |> select(Day) |> distinct(),
@@ -136,98 +129,10 @@ if (!dbExistsTable(con, "META")) {
     conflict = "ignore",
     in_place = TRUE
   )
-}
-
-
-
-
-
-stop()
-##  Get Astropy files  ---------------------------------------------------------
-SUN <- data.table(readRDS(ASTROPY_FL))
-names(SUN)[names(SUN) == "Dist"]      <- "Sun_Dist_Astropy"
-names(SUN)[names(SUN) == "Elevation"] <- "Elevat"
-setorder(SUN, Date)
-stopifnot(length(unique(SUN$Date)) == nrow(SUN))
-SUN <- SUN[as.Date(Date) >= DB_start_date, ]
-
-### FIXME this is for TEST
-{
-  start_test <- as.Date("2020-01-01")
-  test_cnt   <- "~/ZHOST/.test_counter.Rds"
-  if (!file.exists(test_cnt)) {
-    sscn <- 1
-    saveRDS(sscn, test_cnt)
-  } else {
-    sscn <- readRDS(test_cnt) + 1
-    saveRDS(sscn, test_cnt)
-  }
-  end_test <- start_test + 360 + sscn * 1
-  SUN <- SUN[Date < end_test & Date > start_test]
 }
 
 ## Use date as integer
-SUN[, Date := round_date(Date, unit = "second")]
-
-## select what is missing to add
-if (dbExistsTable(con, "LAP")) {
-  SUN <- anti_join(SUN,
-                   tbl(con, "LAP")        |>
-                     select(Date)         |>
-                     filter(!is.na(Date)) |>
-                     collect(),
-                   by = "Date")
-}
-
-##  Create some useful variables
-names(SUN)[names(SUN) == "Dist"] <- "Sun_Dist_Astropy"
-SUN <- SUN |> relocate(Date) |> data.table()
-SUN[, month := month(  as.POSIXct(SUN$Date, origin = "1970-01-01"))]
-SUN[, year  := year(   as.POSIXct(SUN$Date, origin = "1970-01-01"))]
-SUN[, doy   := yday(   as.POSIXct(SUN$Date, origin = "1970-01-01"))]
-SUN[, Day   := as.Date(as.POSIXct(SUN$Date, origin = "1970-01-01"))]
-SUN[, SZA   := 90 - Elevat]
-SUN[Azimuth <= 180, preNoon := TRUE ]
-SUN[Azimuth >  180, preNoon := FALSE]
-
-##  Info display
-cat(paste(
-  Script.ID,
-  ":",
-  unique(as.Date(SUN$Date)
-  )),
-  sep = "\n")
-
-##  Add data  ------------------------------------------------------------------
-if (!dbExistsTable(con, "LAP")) {
-  ## Create new table
-  cat("\n Initialize table 'LAP' \n\n")
-  dbWriteTable(con, "LAP", SUN)
-  ## indexing will block drop of columns for now!!
-  # db_create_index(con, "LAP", columns = "Date", unique = TRUE)
-} else {
-  ## Append new data
-  cat("\n Add data to 'LAP' \n\n")
-  dbWriteTable(con, "LAP", SUN, append = TRUE)
-}
-
-##  Create all corresponding metadata days
-if (!dbExistsTable(con, "META")) {
-
-  ## FIXME should be done in pure SQL
-  init <- tbl(con, "LAP") |> select(Day) |> distinct() |> collect() |> data.table()
-
-  dbWriteTable(con, "META", init)
-} else {
-  ##  Add new dates
-  rows_insert(
-    tbl(con, "META"),
-    tbl(con, "LAP") |> select(Day) |> distinct(),
-    by = "Day",
-    conflict = "ignore",
-    in_place = TRUE
-  )
-}
+# SUN[, Date := round_date(Date, unit = "second")]
 
 ##  Checks  --------------------------------------------------------------------
 stopifnot(tbl(con, "LAP") |> filter(is.na(Date))    |> collect() |> nrow() == 0)
@@ -242,6 +147,8 @@ if (all(tbl(con, "LAP") |> select(Date) |> collect() |> pull() |> diff() == 1)) 
 
 ## Info display
 if (interactive()) {
+  fs::file_size(DB_DUCK)
+
   tbl(con, "LAP") |> tally()
   tbl(con, "LAP") |> glimpse()
 
