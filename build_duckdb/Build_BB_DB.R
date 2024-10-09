c70f5db319e4e489221af3fe7bae826238665c71
thanasisn <11601717+thanasisn@users.noreply.github.com>
2024-10-09 19:08:33 +0300

    Commit 2024-10-09 19:08

--- build_duckdb/Build_BB_DB.R
+++ build_duckdb/Build_BB_DB.R
@@ -20,37 +20,38 @@ renv::load("~/BBand_LAP")
 ##  Prepare sun data for the database  -----------------------------------------
 source("~/BBand_LAP/parameters/sun/create_sun_data.R"           )
 
-
-##  Import raw data from instruments  ------------------------------------------
-source("~/BBand_LAP/build_duckdb/Build_DB_01_initialize.R"      )
-source("~/BBand_LAP/build_duckdb/Build_DB_02_cm21.R"            )
-source("~/BBand_LAP/build_duckdb/Build_DB_03_chp1.R"            )
-source("~/BBand_LAP/build_duckdb/Build_DB_04_chp1_SNC.R"        )
-source("~/BBand_LAP/build_duckdb/Build_DB_05_chp1_TMP.R"        )
-source("~/BBand_LAP/build_duckdb/Build_DB_06_cm21inclined.R"    )
-source("~/BBand_LAP/build_duckdb/Build_DB_16_cm21_TOT.R"        )
-
-##  Flag bad data  -------------------------------------------------------------
-source("~/BBand_LAP/build_duckdb/Build_DB_30_exclude_ranges.R"  )
-
-
-
-##  Raw to actual data  --------------------------------------------------------
-
-## __ Apply dark offset and convert to radiation  ------------------------------
-source("~/BBand_LAP/build_duckdb/Build_DB_42_cm21_dark_radiat.R")
-source("~/BBand_LAP/build_duckdb/Build_DB_43_chp1_dark_radiat.R")
-
-## Second pass to construct missing dark !
-source("~/BBand_LAP/build_duckdb/Build_DB_42_cm21_dark_radiat.R")
-source("~/BBand_LAP/build_duckdb/Build_DB_43_chp1_dark_radiat.R")
-
-## __ Extra process for CHP-1 temperature  -------------------------------------
-# source("~/BBand_LAP/build_duckdb/Build_DB_44_chp1_temp_correc.R")
-
-## Add data from other sources  ------------------------------------------------
-source("~/BBand_LAP/build_duckdb/Import_50_TSI.R"               )
-source("~/BBand_LAP/build_duckdb/Import_51_Pressure.R"          )
+# stop("NO STORAGE ON SAGAN")
+#
+# ##  Import raw data from instruments  ------------------------------------------
+# source("~/BBand_LAP/build_duckdb/Build_DB_01_initialize.R"      )
+# source("~/BBand_LAP/build_duckdb/Build_DB_02_cm21.R"            )
+# source("~/BBand_LAP/build_duckdb/Build_DB_03_chp1.R"            )
+# source("~/BBand_LAP/build_duckdb/Build_DB_04_chp1_SNC.R"        )
+# source("~/BBand_LAP/build_duckdb/Build_DB_05_chp1_TMP.R"        )
+# source("~/BBand_LAP/build_duckdb/Build_DB_06_cm21inclined.R"    )
+# source("~/BBand_LAP/build_duckdb/Build_DB_16_cm21_TOT.R"        )
+#
+# ##  Flag bad data  -------------------------------------------------------------
+# source("~/BBand_LAP/build_duckdb/Build_DB_30_exclude_ranges.R"  )
+#
+#
+#
+# ##  Raw to actual data  --------------------------------------------------------
+#
+# ## __ Apply dark offset and convert to radiation  ------------------------------
+# source("~/BBand_LAP/build_duckdb/Build_DB_42_cm21_dark_radiat.R")
+# source("~/BBand_LAP/build_duckdb/Build_DB_43_chp1_dark_radiat.R")
+#
+# ## Second pass to construct missing dark !
+# source("~/BBand_LAP/build_duckdb/Build_DB_42_cm21_dark_radiat.R")
+# source("~/BBand_LAP/build_duckdb/Build_DB_43_chp1_dark_radiat.R")
+#
+# ## __ Extra process for CHP-1 temperature  -------------------------------------
+# # source("~/BBand_LAP/build_duckdb/Build_DB_44_chp1_temp_correc.R")
+#
+# ## Add data from other sources  ------------------------------------------------
+# source("~/BBand_LAP/build_duckdb/Import_50_TSI.R"               )
+# source("~/BBand_LAP/build_duckdb/Import_51_Pressure.R"          )
 
 
 
