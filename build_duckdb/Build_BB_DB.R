44334dde2c27dbe3fa5a20c774aa7741c9ab7281
thanasisn <11601717+thanasisn@users.noreply.github.com>
2024-10-11 08:13:29 +0300

    Commit 2024-10-11 08:13

--- build_duckdb/Build_BB_DB.R
+++ build_duckdb/Build_BB_DB.R
@@ -45,10 +45,10 @@ source("~/BBand_LAP/build_duckdb/Build_DB_43_chp1_dark_radiat.R")
 
 # ## __ Extra process for CHP-1 temperature  -------------------------------------
 # # source("~/BBand_LAP/build_duckdb/Build_DB_44_chp1_temp_correc.R")
-#
-# ## Add data from other sources  ------------------------------------------------
-# source("~/BBand_LAP/build_duckdb/Import_50_TSI.R"               )
-# source("~/BBand_LAP/build_duckdb/Import_51_Pressure.R"          )
+
+## Add data from other sources  ------------------------------------------------
+source("~/BBand_LAP/build_duckdb/Import_50_TSI.R"               )
+source("~/BBand_LAP/build_duckdb/Import_51_Pressure.R"          )
 
 
 
