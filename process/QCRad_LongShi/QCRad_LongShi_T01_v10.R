b276723193c583697e1e08ecc8a4483f68775f62
thanasisn <11601717+thanasisn@users.noreply.github.com>
2024-10-12 18:13:38 +0300

    Commit 2024-10-12 18:13

--- process/QCRad_LongShi/QCRad_LongShi_T01_v10.R
+++ process/QCRad_LongShi/QCRad_LongShi_T01_v10.R
@@ -162,29 +162,26 @@ if (Sys.info()["nodename"] == "sagan") {
       ))
   res <- update_table(con, ADD, "LAP", "Date")
 
-stop("fix this")
   ## __ Flag global  -----------------------------------------------------------
   ## create a reference for global
   ## TODO create Glo_max_ref column
   ADD <- tbl(con, "LAP")             |>
     filter(Elevat > QS$sun_elev_min) |>
     filter(!is.na(TSI_TOA))          |>
-    mutate(ss = QS$glo_SWdn_amp * cos(SZA*pi/180)^1.2) |>
-    select(ss, TSI_TOA)
-
-    ff <- tbl(con, "LAP") |> select(TSI_TOA, SZA) |> collect() |> data.table()
-
-    ## this is possible
-    ff[, test := TSI_TOA * QS$glo_SWdn_amp * cos(SZA*pi/180)^1.2 + QS$glo_SWdn_off]
-    summary(ff)
-
+    # mutate(ss = QS$glo_SWdn_amp * cos(SZA*pi/180)^1.2) |>
+    # select(ss, TSI_TOA)
+    # ff <- tbl(con, "LAP") |> select(TSI_TOA, SZA) |> collect() |> data.table()
+    #
+    # ## this is possible
+    # ff[, test := TSI_TOA * QS$glo_SWdn_amp * cos(SZA*pi/180)^1.2 + QS$glo_SWdn_off]
+    # ## to_arrow?
       mutate(
 
       Glo_max_ref := case_when(
         TSI_TOA * QS$glo_SWdn_amp * cos(SZA*pi/180)^1.2 + QS$glo_SWdn_off >  9000 ~ 9000,
         TSI_TOA * QS$glo_SWdn_amp * cos(SZA*pi/180)^1.2 + QS$glo_SWdn_off <= 9000 ~ TSI_TOA * QS$glo_SWdn_amp * cos(SZA*pi/180)^1.2 + QS$glo_SWdn_off
 
-      ))
+      )) |> collect()
   res <- update_table(con, ADD, "LAP", "Date")
 
   ## apply test
