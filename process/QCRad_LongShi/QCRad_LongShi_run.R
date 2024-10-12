bdefa8842947deae0e8f60bb77d65807a8a4be5b
thanasisn <11601717+thanasisn@users.noreply.github.com>
2024-10-12 07:08:00 +0300

    Commit 2024-10-12 07:08

---
+++ process/QCRad_LongShi/QCRad_LongShi_run.R
@@ -0,0 +1,21 @@
+#!/opt/R/4.2.3/bin/Rscript
+# /* Copyright (C) 2022-2023 Athanasios Natsis <natsisphysicist@gmail.com> */
+
+## __ Set environment  ---------------------------------------------------------
+closeAllConnections()
+rm(list = (ls()[ls() != ""]))
+Sys.setenv(TZ = "UTC")
+Script.Name <- "~/BBand_LAP/process/QCRad_LongShi/QCRad_LongShi_run.R"
+renv::load("~/BBand_LAP")
+
+
+##  Create some new variables  -------------------------------------------------
+source("~/BBand_LAP/process/QCRad_LongShi/QCRad_LongShi_T00_v10.R"      )
+
+##  Run test  ------------------------------------------------------------------
+source("~/BBand_LAP/process/QCRad_LongShi/QCRad_LongShi_T01_v10.R"      )
+
+
+
+cat("\n\nEND of QCRad LongShi \n\n")
+
