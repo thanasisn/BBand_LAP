c06e6a5c78ade0fc68b22980525fc61b4135163f
thanasisn <11601717+thanasisn@users.noreply.github.com>
2024-11-01 06:07:42 +0200

    Commit 2024-11-01 06:07

--- build_duckdb/Build_DB_42_cm21_dark_radiat.R
+++ build_duckdb/Build_DB_42_cm21_dark_radiat.R
@@ -104,16 +104,13 @@ for (ad in dayslist) {
 
   ## use only valid data for dark calculation
   daydata <-
-    tbl(con, "LAP")                       |>
-    filter(Day == ad)                     |>  ## this day only
-    filter(!is.na(CM21_sig))              |>  ## valid measurements
-#    filter(is.na(cm21_bad_data_flag))     |>  ## not bad data
-    filter(cm21_bad_data_flag  == "pass") |>  ## not bad data
-    filter(cm21_sig_limit_flag == "pass") |>  ## acceptable values range
+    tbl(con, "LAP")                                     |>
+    filter(Day == ad)                                   |> ## this day only
+    filter(!is.na(CM21_sig))                            |> ## valid measurements
+    filter(cm21_bad_data_flag  %in% c("pass", "empty")) |> ## not bad data
+    filter(cm21_sig_limit_flag %in% c("pass", "empty")) |> ## acceptable values range
     collect() |>
     data.table()
-
-  stop("check here")
 
   ## Ignore bad and missing data
   if (nrow(daydata) == 0) {
