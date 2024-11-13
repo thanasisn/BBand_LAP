bed949e6d043832b962caa3ef8356e45909fe8e9
thanasisn <11601717+thanasisn@users.noreply.github.com>
2024-11-13 22:13:33 +0200

    Commit 2024-11-13 22:13

---
+++ execution/P02_TSI.sh
@@ -0,0 +1,41 @@
+#!/usr/bin/env bash
+## created on 2024-11-05
+
+####  Get parse and upload LGTS sounding data from uwyo
+
+exec 9>"/dev/shm/$(basename $0).lock"
+if ! flock -n 9  ; then
+    echo "another instance of $0 is running";
+    exit 1
+fi
+
+ldir="$HOME/BBand_LAP/REPORTS/LOGs/TSI"
+mkdir -p "$ldir"
+LOG_FILE="$ldir/$(basename "$0")_$(date +%F_%R).log"
+ERR_FILE="$ldir/$(basename "$0")_$(date +%F_%R).err"
+exec  > >(tee -i "${LOG_FILE}")
+exec 2> >(tee -i "${ERR_FILE}" >&2)
+## send output directly to files
+# exec  > "${LOG_FILE}"
+# exec 2> "${ERR_FILE}"
+TIC=$(date +"%s")
+
+: "${ID:=$(hostname)}"
+SCRIPT="$(basename "$0")"
+
+info() { echo ; echo "$(date +'%F %T') ::${SCRIPT}::${ID}:: $* ::" ; echo ; }
+
+echo "###################################"
+echo "####    $(date +"%F %T")    ####"
+echo "###################################"
+
+## ignore errors
+set +e
+
+# info "##  Scrap uwyo data for Thessaloniki"
+# "$HOME/BBand_LAP/parameters/uwyo/scrap_uwyo.sh"
+
+
+info "#### END $0 ####"
+TAC=$(date +"%s"); dura="$( echo "scale=6; ($TAC-$TIC)/60" | bc)"
+printf "%s %-10s %-10s %-10s %f\n" "$(date +"%F %H:%M:%S")" "$HOSTNAME" "$USER" "$(basename $0)" "$dura"
