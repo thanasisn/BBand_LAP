854bb174c40730a62cb3f0aa1ba7267a73b6583b
thanasisn <11601717+thanasisn@users.noreply.github.com>
2024-11-26 12:13:33 +0200

    Commit 2024-11-26 12:13

---
+++ parameters/TSI/Build_TSI.R
@@ -0,0 +1,18 @@
+#!/opt/R/4.2.3/bin/Rscript
+# /* Copyright (C) 2022-2023 Athanasios Natsis <natsisphysicist@gmail.com> */
+
+## __ Set environment  ---------------------------------------------------------
+rm(list = (ls()[ls() != ""]))
+Sys.setenv(TZ = "UTC")
+tic <- Sys.time()
+Script.Name <- "~/BBand_LAP/parameters/TSI/Build_TSI.R"
+renv::load("~/BBand_LAP")
+
+##  Get and parse NOAA TSI  ----------------------------------------------------
+source("~/BBand_LAP/parameters/TSI/Read_raw_TSI_NOAA.R"  )
+##  Create TSI for LAP  --------------------------------------------------------
+source("~/BBand_LAP/parameters/TSI/Create_LAP_TSI.R"     )
+
+
+tac <- Sys.time()
+cat(sprintf("%s %s@%s %s %f mins\n\n",Sys.time(),Sys.info()["login"],Sys.info()["nodename"],Script.Name,difftime(tac,tic,units="mins")))
