0dbcadf7f0e95a3526d65c41e22b3cf3a1607999
thanasisn <11601717+thanasisn@users.noreply.github.com>
2024-12-06 19:07:37 +0200

    Commit 2024-12-06 19:07

--- parameters/TSI/Build_TSI.R
+++ parameters/TSI/Build_TSI.R
@@ -15,11 +15,28 @@ output_dir <- "~/BBand_LAP/REPORTS/REPORTS_duck/"
 source("~/BBand_LAP/parameters/TSI/00_download_TSI.R"      )
 
 ##  Parse TSI to data base  ----------------------------------------------------
-source("~/BBand_LAP/parameters/TSI/01_Read_raw_TSI_NOAA.R" )
-source("~/BBand_LAP/parameters/TSI/02_Read_raw_TSI_TSIS.R" )
+# source("~/BBand_LAP/parameters/TSI/01_Read_raw_TSI_NOAA.R" )
+# source("~/BBand_LAP/parameters/TSI/02_Read_raw_TSI_TSIS.R" )
+try({
+  cat("\n\n 01. Read raw TSI NOAA \n")
+  rmarkdown::render(input      = "~/BBand_LAP/parameters/TSI/01_Read_raw_TSI_NOAA.R",
+                    output_dir = output_dir)
+})
+try({
+  cat("\n\n 02. Read raw TSI TSIS \n")
+  rmarkdown::render(input      = "~/BBand_LAP/parameters/TSI/02_Read_raw_TSI_TSIS.R",
+                    output_dir = output_dir)
+})
+
 
 ##  Create TSI for LAP  --------------------------------------------------------
-source("~/BBand_LAP/parameters/TSI/10_Create_LAP_TSI.R"    )
+# source("~/BBand_LAP/parameters/TSI/10_Create_LAP_TSI.R" )
+# source("~/BBand_LAP/parameters/TSI/11_Extend_LAP_TSI.R" )
+try({
+  cat("\n\n 10. Create LAP TSI \n")
+  rmarkdown::render(input      = "~/BBand_LAP/parameters/TSI/10_Create_LAP_TSI.R",
+                    output_dir = output_dir)
+})
 try({
   cat("\n\n 11. Extend LAP TSI \n")
   rmarkdown::render(input      = "~/BBand_LAP/parameters/TSI/11_Extend_LAP_TSI.R",
@@ -29,6 +46,7 @@ try({
 
 ##  This will be removed  ------------------------------------------------------
 source("~/BBand_LAP/parameters/TSI/30_Export_LAP_TSI_legacy.R")
+
 
 tac <- Sys.time()
 cat(sprintf("%s %s@%s %s %f mins\n\n",Sys.time(),Sys.info()["login"],Sys.info()["nodename"],Script.Name,difftime(tac,tic,units="mins")))
