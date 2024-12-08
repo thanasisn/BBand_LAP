d3fab0540c9b770876fd55ef34d1d5f3065ebdfe
thanasisn <11601717+thanasisn@users.noreply.github.com>
2024-12-08 20:13:35 +0200

    Commit 2024-12-08 20:13

--- parameters/weather/Build_weather.R
+++ parameters/weather/Build_weather.R
@@ -12,9 +12,6 @@ renv::load("~/BBand_LAP", quiet = TRUE)
 output_dir <- "~/BBand_LAP/REPORTS/REPORTS/Parameters_weather/"
 dir.create(output_dir, showWarnings = FALSE, recursive = TRUE)
 
-##  Run old constructor  -------------------------------------------------------
-source("~/BBand_LAP/parameters/weather/combine_weather_pressure_v1.R")
-
 ##  Parse data from Davis  -----------------------------------------------------
 try({
   cat("\n\n 01. Read LAP davis \n")
@@ -23,13 +20,20 @@ try({
 })
 
 ##  Create pressure for LAP  ---------------------------------------------------
-# source("~/BBand_LAP/parameters/weather/10_Create_LAP_pressure.R" )
+# # source("~/BBand_LAP/parameters/weather/10_Create_LAP_pressure.R" )
+# try({
+#   cat("\n\n 10. Create LAP pressure \n")
+#   rmarkdown::render(input      = "~/BBand_LAP/parameters/weather/10_Create_LAP_pressure.R",
+#                     output_dir = output_dir)
+# })
+
+##  Run old constructor  -------------------------------------------------------
+# source("~/BBand_LAP/parameters/weather/combine_weather_pressure_v1.R")
 try({
-  cat("\n\n 10. Create LAP pressure \n")
-  rmarkdown::render(input      = "~/BBand_LAP/parameters/weather/10_Create_LAP_pressure.R",
+  cat("\n\n 00. combine weather pressure \n")
+  rmarkdown::render(input      = "~/BBand_LAP/parameters/weather/combine_weather_pressure_v1.R",
                     output_dir = output_dir)
 })
-
 
 tac <- Sys.time()
 cat(sprintf("%s %s@%s %s %f mins\n\n",Sys.time(),Sys.info()["login"],Sys.info()["nodename"],Script.Name,difftime(tac,tic,units="mins")))
