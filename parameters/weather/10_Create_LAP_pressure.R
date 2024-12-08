57ecc4c2a61dfb2e6b934985af32bed01533de2c
thanasisn <11601717+thanasisn@users.noreply.github.com>
2024-12-08 09:07:59 +0200

    Commit 2024-12-08 09:07

--- parameters/weather/10_Create_LAP_pressure.R
+++ parameters/weather/10_Create_LAP_pressure.R
@@ -59,18 +59,12 @@ WUithess2_f <- "/home/athan/DATA/Wunderground/ITHESSAL2.Rds"
 #+ include=T, echo=F, results="asis"
 cat("\n# Create pressure data for LAP\n\n")
 
-file.mtime(davis_lap) > file.mtime(DB_PRESSURE)
-
+#+ include=T, echo=F
 pressure_limit_low  <- 970
 
-#+ include=F
-## load all data
-DILA <- readRDS(davis_lap)
 
-stop()
 ##  Open dataset  --------------------------------------------------------------
 con <- dbConnect(duckdb(dbdir = DB_PRESSURE))
-
 
 if (!dbExistsTable(con, "PRESSURE_RAW")) {
   cat("\n Initialize table", "PRESSURE_RAW", "\n\n")
@@ -134,197 +128,200 @@ if (!dbExistsTable(con, "PRESSURE_RAW")) {
 
   ## Data from roof Davis of electronics department
 
-  DAVI <- readRDS(davis_elect)
-  DAVI
+  DAVI <- data.table(readRDS(davis_elect))
+  DAVI <- DAVI[, .(Date, Bar)]
 
   cat(
     "\ndavis_elect: Ignoring",
-    DITH[barometer < pressure_limit_low, .N],
+    DAVI[Bar < pressure_limit_low, .N],
     "pressure points below", pressure_limit_low,
     "hPa\n\n"
   )
 
+  DAVI <- DAVI |>
+    rename(Pressure_davis_elect = Bar) |>
+    filter(Pressure_davis_elect > pressure_limit_low) |>
+    arrange(Date)
 
+  stopifnot(all(duplicated(DAVI$Date)) == FALSE)
 
+  res <- insert_table(con, DAVI, "PRESSURE_RAW", "Date")
+  rm(DAVI); d <- gc()
 }
 
-RAW <- tbl(con, "PRESSURE_RAW")
-RAW
+##  Add active data  -----------------------------------------------------------
 
+##  This will work after a davis export
+
+if (dbExistsTable(con, "PRESSURE_RAW") &
+    file.mtime(davis_lap) > file.mtime(DB_PRESSURE)) {
+
+  ## __ LAP davis on the roof  -------------------------------------------------
+
+  DILA <- readRDS(davis_lap)
+  DILA <- DILA[, Date, pressure]
+  DILA <- DILA[!is.na(pressure)]
+
+  cat(
+    "\ndavis_lap: Ignoring",
+    DILA[pressure < pressure_limit_low, .N],
+    "pressure points below", pressure_limit_low,
+    "hPa\n\n"
+  )
+
+  DILA <- DILA |>
+    rename(Pressure_davis_lap = pressure) |>
+    filter(Pressure_davis_lap > pressure_limit_low) |>
+    arrange(Date)
+
+  stopifnot(all(duplicated(DILA$Date)) == FALSE)
+
+  res <- insert_table(con, DILA, "PRESSURE_RAW", "Date")
+  rm(DILA); d <- gc()
+}
+
+
+RAW <- tbl(con, "PRESSURE_RAW")
+RAW |> tally()
+RAW |> summarise(min(Date, na.rm = TRUE), max(Date, na.rm = TRUE))
 
 RAW |>
   ggplot() +
-  geom_point(aes(x = Date, y = Pressure_WUithess2), color = 2, size = 0.6) +
-  geom_point(aes(x = Date, y = Pressure_DIithess2), color = 3, size = 0.6)
+  geom_point(aes(x = Date, y = Pressure_WUithess2),   color = 2, size = 0.5) +
+  geom_point(aes(x = Date, y = Pressure_DIithess2),   color = 3, size = 0.5) +
+  geom_point(aes(x = Date, y = Pressure_davis_elect), color = 4, size = 0.5) +
+  geom_point(aes(x = Date, y = Pressure_davis_lap),   color = 5, size = 0.5)
 
 
 
-##  Add new dates to main table  -----------------------------------------------
+##  Create composite pressure  -------------------------------------------------
 #'
-#' ## Fill the dates we care to fill with TSI data
-#'
-start_date   <- DB_start_date - 1  ## go back one day to fill with interpolation
-sun_up_limit <- -3                 ## limit data creation to daylight only
-
-SUN <- tbl(sun, "params")                             |>
-  filter(!is.na(AsPy_Elevation) & Date >= start_date) |>
-  rename(Elevat           = "AsPy_Elevation")         |>
-  filter(Elevat > sun_up_limit)                       |>
-  select(Date)
-
-if (!dbExistsTable(con, "LAP_TSI")) {
-  ## Create new table
-  cat("\n Initialize table", "LAP_TSI", "\n\n")
-  ## create table and date variable with pure SQL call
-  dbExecute(con, paste("CREATE TABLE", "LAP_TSI", "(Date TIMESTAMP)"))
-  ## fill table
-  res <- insert_table(con,
-                      SUN |> select(Date) |> arrange(Date),
-                      "LAP_TSI", "Date")
-} else {
-  ## Append new data if needed
-  SUN <- anti_join(SUN |>
-                     select(Date),
-                   tbl(con, "LAP_TSI") |>
-                     select(Date)  |>
-                     filter(!is.na(Date)),
-                   by   = "Date",
-                   copy = TRUE)
-
-  if (SUN |> tally() |> pull() > 0) {
-    cat("\n Add more dates to", "LAP_TSI", "\n\n")
-    cat(paste(Script.ID, ":",
-              SUN |> tally() |> pull(),
-              "New rows"), "\n")
-
-    res <- insert_table(con,
-                        SUN |> select(Date) |> arrange(Date),
-                        "LAP_TSI", "Date")
-  }
-}
-
-##  Fill LAP TSI DATA  ---------------------------------------------------------
-
-## __ Insert raw NOAA  ---------------------------------------------------------
-#'
-#' ## Insert raw NOAA values to the main table
+#' ## Fill the composite pressure form raw pressure
 #'
 
-## ADD row values for LAP
-RAW <- tbl(con, "TSI_NOAA")                |>
-  mutate(Source = "NOAA_RAW")              |>
-  select(Time, TSI, Source, file_Creation) |>
-  rename(Updated = "file_Creation")        |>
-  rename(Date = "Time")
 
-if (!any(tbl(con, "LAP_TSI") |> colnames() %in% "Source")) {
-  cat("Inialiaze table", "LAP_TSI", "\n\n")
-  ## Add raw values
-  res <- update_table(con, RAW, "LAP_TSI", "Date")
-}
-
-TEST <- tbl(con, "LAP_TSI")        |>
-  filter(Source == "NOAA_RAW") |>
-  select(Date, TSI)
-
-##  Update with newer data
-if (anti_join(TEST, RAW) |> tally() |> pull() == 0) {
-  cat("No new data from NOAA\n\n")
-} else {
-  cat("New data from NOAA\n\n")
-  remove_column(con, "LAP_TSI", "TSI")
-  remove_column(con, "LAP_TSI", "TSI_TOA")
-  remove_column(con, "LAP_TSI", "TSI_LAP")
-  ## Add raw values
-  res <- update_table(con, RAW, "LAP_TSI", "Date")
-}
-
-## __ Interpolate NOAA  ---------------------------------------------------------
-#'
-#' ## Fill TSI with interpolated values from raw NOAA.
-#'
-#'  Create a function than can fill any date in the date range
-#'
-
-##  Create interpolation function
-tt <- tbl(con, "LAP_TSI") |> filter(Source == "NOAA_RAW") |>
-  collect() |> data.table()
-tsi_fun <- approxfun(
-  x      = tt$Date,
-  y      = tt$TSI,
-  method = "linear",
-  rule   = 1,
-  ties   = mean
-)
-
-## Fill raw with interpolation
-NEW <- tbl(con, "LAP_TSI")
-
-## Fill with interpolated data
-yearstofill <- NEW           |>
-  filter(is.na(TSI))         |>
-  mutate(year = year(Date))  |>
-  select(year) |> distinct() |> pull()
-
-for (ay in yearstofill) {
-  some <- NEW |> filter(year(Date) == ay) |>
-    filter(is.na(TSI)) |> select(Date) |> collect() |> data.table()
-  some[, TSI     := tsi_fun(Date)]
-  some[, Source  := "NOAA_INTERP"]
-  some[, Updated := Sys.time()   ]
-  ## write only when needed
-  some <- some[!is.na(TSI)]
-  if (nrow(some) > 0) {
-    cat(paste(Script.ID, ":",
-              "Interpolate TSI for", ay), "\n")
-    res <- update_table(con, some, "LAP_TSI", "Date")
-  }
-}
-
-## __ Calculate other TSI values for LAP  --------------------------------------
-#'
-#' ## Create values of TSI at TOA and LAP ground for all
-#'
-
-## Fill TOA and LAP ground
-make_new_column(con = con, table = "LAP_TSI", "TSI_TOA")
-make_new_column(con = con, table = "LAP_TSI", "TSI_LAP")
-NEW <- tbl(con, "LAP_TSI")
-
-yearstofill <- NEW |>
-  filter(Date > DB_start_date)            |>
-  filter(is.na(TSI_TOA) | is.na(TSI_LAP)) |>
-  mutate(year = year(Date))               |>
-  select(year) |> distinct() |> pull()
-
-for (ay in yearstofill) {
-  some <- NEW |> filter(year(Date) == ay)   |>
-    filter(is.na(TSI_TOA) | is.na(TSI_LAP)) |>
-    select(Date, TSI)
-
-  SUN <- tbl(sun, "params") |>
-    filter(year(Date) == ay) |>
-    filter(!is.na(AsPy_Elevation) & Date >= DB_start_date) |>
-    rename(Sun_Dist_Astropy = "AsPy_Dist")       |>
-    rename(Elevat           = "AsPy_Elevation")  |>
-    mutate(SZA              = 90 - Elevat)       |>
-    select(Date, Sun_Dist_Astropy, SZA)
-
-  ADD <- left_join(some, SUN, copy = T) |>
-    mutate(
-      TSI_TOA = TSI / Sun_Dist_Astropy^2,  ## TSI on LAP TOA
-      TSI_LAP = TSI_TOA * cos(SZA*pi/180)  ## TSI on LAP ground
-    ) |>
-    select(Date, TSI_TOA, TSI_LAP) |>
-    filter(!is.na(TSI_TOA) & !is.na(TSI_LAP))
-
-  ## write only when needed
-  if (ADD |> tally() |> pull() > 0) {
-    cat(paste(Script.ID, ":",
-              "TOA and Ground TSI for", ay), "\n")
-    res <- update_table(con, ADD, "LAP_TSI", "Date")
-  }
-}
+# ##  Fill LAP TSI DATA  ---------------------------------------------------------
+#
+# ## __ Insert raw NOAA  ---------------------------------------------------------
+# #'
+# #' ## Insert raw NOAA values to the main table
+# #'
+#
+# ## ADD row values for LAP
+# RAW <- tbl(con, "TSI_NOAA")                |>
+#   mutate(Source = "NOAA_RAW")              |>
+#   select(Time, TSI, Source, file_Creation) |>
+#   rename(Updated = "file_Creation")        |>
+#   rename(Date = "Time")
+#
+# if (!any(tbl(con, "LAP_TSI") |> colnames() %in% "Source")) {
+#   cat("Inialiaze table", "LAP_TSI", "\n\n")
+#   ## Add raw values
+#   res <- update_table(con, RAW, "LAP_TSI", "Date")
+# }
+#
+# TEST <- tbl(con, "LAP_TSI")        |>
+#   filter(Source == "NOAA_RAW") |>
+#   select(Date, TSI)
+#
+# ##  Update with newer data
+# if (anti_join(TEST, RAW) |> tally() |> pull() == 0) {
+#   cat("No new data from NOAA\n\n")
+# } else {
+#   cat("New data from NOAA\n\n")
+#   remove_column(con, "LAP_TSI", "TSI")
+#   remove_column(con, "LAP_TSI", "TSI_TOA")
+#   remove_column(con, "LAP_TSI", "TSI_LAP")
+#   ## Add raw values
+#   res <- update_table(con, RAW, "LAP_TSI", "Date")
+# }
+#
+# ## __ Interpolate NOAA  ---------------------------------------------------------
+# #'
+# #' ## Fill TSI with interpolated values from raw NOAA.
+# #'
+# #'  Create a function than can fill any date in the date range
+# #'
+#
+# ##  Create interpolation function
+# tt <- tbl(con, "LAP_TSI") |> filter(Source == "NOAA_RAW") |>
+#   collect() |> data.table()
+# tsi_fun <- approxfun(
+#   x      = tt$Date,
+#   y      = tt$TSI,
+#   method = "linear",
+#   rule   = 1,
+#   ties   = mean
+# )
+#
+# ## Fill raw with interpolation
+# NEW <- tbl(con, "LAP_TSI")
+#
+# ## Fill with interpolated data
+# yearstofill <- NEW           |>
+#   filter(is.na(TSI))         |>
+#   mutate(year = year(Date))  |>
+#   select(year) |> distinct() |> pull()
+#
+# for (ay in yearstofill) {
+#   some <- NEW |> filter(year(Date) == ay) |>
+#     filter(is.na(TSI)) |> select(Date) |> collect() |> data.table()
+#   some[, TSI     := tsi_fun(Date)]
+#   some[, Source  := "NOAA_INTERP"]
+#   some[, Updated := Sys.time()   ]
+#   ## write only when needed
+#   some <- some[!is.na(TSI)]
+#   if (nrow(some) > 0) {
+#     cat(paste(Script.ID, ":",
+#               "Interpolate TSI for", ay), "\n")
+#     res <- update_table(con, some, "LAP_TSI", "Date")
+#   }
+# }
+#
+# ## __ Calculate other TSI values for LAP  --------------------------------------
+# #'
+# #' ## Create values of TSI at TOA and LAP ground for all
+# #'
+#
+# ## Fill TOA and LAP ground
+# make_new_column(con = con, table = "LAP_TSI", "TSI_TOA")
+# make_new_column(con = con, table = "LAP_TSI", "TSI_LAP")
+# NEW <- tbl(con, "LAP_TSI")
+#
+# yearstofill <- NEW |>
+#   filter(Date > DB_start_date)            |>
+#   filter(is.na(TSI_TOA) | is.na(TSI_LAP)) |>
+#   mutate(year = year(Date))               |>
+#   select(year) |> distinct() |> pull()
+#
+# for (ay in yearstofill) {
+#   some <- NEW |> filter(year(Date) == ay)   |>
+#     filter(is.na(TSI_TOA) | is.na(TSI_LAP)) |>
+#     select(Date, TSI)
+#
+#   SUN <- tbl(sun, "params") |>
+#     filter(year(Date) == ay) |>
+#     filter(!is.na(AsPy_Elevation) & Date >= DB_start_date) |>
+#     rename(Sun_Dist_Astropy = "AsPy_Dist")       |>
+#     rename(Elevat           = "AsPy_Elevation")  |>
+#     mutate(SZA              = 90 - Elevat)       |>
+#     select(Date, Sun_Dist_Astropy, SZA)
+#
+#   ADD <- left_join(some, SUN, copy = T) |>
+#     mutate(
+#       TSI_TOA = TSI / Sun_Dist_Astropy^2,  ## TSI on LAP TOA
+#       TSI_LAP = TSI_TOA * cos(SZA*pi/180)  ## TSI on LAP ground
+#     ) |>
+#     select(Date, TSI_TOA, TSI_LAP) |>
+#     filter(!is.na(TSI_TOA) & !is.na(TSI_LAP))
+#
+#   ## write only when needed
+#   if (ADD |> tally() |> pull() > 0) {
+#     cat(paste(Script.ID, ":",
+#               "TOA and Ground TSI for", ay), "\n")
+#     res <- update_table(con, ADD, "LAP_TSI", "Date")
+#   }
+# }
 
 #+ Clean_exit, echo=FALSE
 dbDisconnect(con, shutdown = TRUE); rm(con)
