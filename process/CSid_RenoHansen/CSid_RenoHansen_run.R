1767d5c438ee2709b4646a22b6d97ab717487d32
thanasisn <11601717+thanasisn@users.noreply.github.com>
2024-12-21 20:07:34 +0200

    Commit 2024-12-21 20:07

--- process/CSid_RenoHansen/CSid_RenoHansen_run.R
+++ process/CSid_RenoHansen/CSid_RenoHansen_run.R
@@ -8,12 +8,15 @@ Sys.setenv(TZ = "UTC")
 Script.Name <- "~/BBand_LAP/process/CSid_RenoHansen/CSid_RenoHansen_run.R"
 renv::load("~/BBand_LAP")
 
+output_dir <- "~/BBand_LAP/REPORTS/REPORTS/CSid_RenoHansen/"
+dir.create(output_dir, showWarnings = FALSE, recursive = TRUE)
 
 ##  Run training  --------------------------------------------------------------
-
-# make each filter a script with parameters
-# use the set of filters for training and application of CS
-
+source("~/BBand_LAP/process/CSid_RenoHansen/CSid_RenoHansen_01_alldata_optim_v15.R")
+try({
+  rmarkdown::render(input      = "~/BBand_LAP/CSid_RenoHansen/CSid_RenoHansen_01_alldata_optim_v15.R",
+                    output_dir = output_dir)
+})
 
 ##  Run tests  -----------------------------------------------------------------
 
