91df42141ebad12d45b59ef34679232a0800c0f8
thanasisn <11601717+thanasisn@users.noreply.github.com>
2025-01-23 12:13:35 +0200

    Commit 2025-01-23 12:13

--- tools/Get_data_from_sirena.sh
+++ tools/Get_data_from_sirena.sh
@@ -15,58 +15,90 @@ SOURCE="/media/sirena_lapdata_ro"
 if mountpoint -q "$SOURCE" ; then
   echo "Sirena is mounted"
 else
-    echo ""
-    echo "No DATA mount point found!!"
-    notify-send          -u critical "1: SIRENA NOT MOUNTED" "Can not get source files"
-    pub_notifications.py -u critical "2: SIRENA NOT MOUNTED" "Can not get source files"
+  echo ""
+  echo "No DATA mount point found!!"
+  notify-send          -u critical "1: SIRENA NOT MOUNTED" "Can not get source files"
+  pub_notifications.py -u critical "2: SIRENA NOT MOUNTED" "Can not get source files"
 fi
 
 ##  Get data  ------------------------------------------------------------------
 
 ##  Get all broadband channels
 
+target_base="$HOME/DATA_RAW/Bband/"
+## use full paths
+folders=(
+	"/media/sirena_lapdata_ro/archive/Bband/TUVR_LAP.GLB/"   # 0
+	"/media/sirena_lapdata_ro/archive/Bband/CM21_LAP.INC/"   # 1
+	"/media/sirena_lapdata_ro/archive/Bband/PIR_lap.GLB/"    # 2
+	"/media/sirena_lapdata_ro/archive/Bband/CHP1_lap.DIR/"   # 3
+	"/media/sirena_lapdata_ro/archive/Bband/YES_LAP.GLB/"    # 4
+	"/media/sirena_lapdata_ro/archive/Bband/EKO_LAP.GLB/"    # 5
+	"/media/sirena_lapdata_ro/archive/Bband/AC21_LAP.GLB/"   # 6
+	"/media/sirena_lapdata_ro/archive/Bband/YES_LAP.DIF/"    # 7
+)
+
+for from in "${folders[@]}"; do
+  ## test source and create target
+  target="$target_base/$(basename "$from")"
+  [ ! -d "$from" ] && echo "Folder not there: $from" && continue
+
+  echo
+  echo "-------------------------------------------------------------------"
+  echo "  Doing: $from"
+  echo "- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -"
+  echo "$from -> $target"
+
+  rsync -arvth          \
+    --include '*/'      \
+    --include '*.LAP'   \
+    --include '*.lap'   \
+    --include '*.ori'   \
+    --exclude '*'       \
+    "$from/"            \
+    "$target"
+done
 
 
 
-
-echo "- - - - - - - - - - - - - - - - - - - -"
-echo "Get signal files 'LAP' of CM-21 global"
-rsync -arvth                                   \
-    --include '*/'                             \
-    --include '*.LAP'                          \
-    --include '*.lap'                          \
-    --include '*.ori'                          \
-    --exclude '*'                              \
-    "$SOURCE/archive/Bband/AC21_LAP.GLB/"      \
-    "$HOME/DATA_RAW/Bband/AC21_LAP.GLB"
-
-echo "- - - - - - - - - - - - - - - - - - - -"
-echo "Get signal files 'LAP' of CM-21 inclined"
-rsync -arvth                                   \
-    --include '*/'                             \
-    --include '*.LAP'                          \
-    --include '*.lap'                          \
-    --include '*.ori'                          \
-    --exclude '*'                              \
-    "$SOURCE/archive/Bband/CM21_LAP.INC/"      \
-    "$HOME/DATA_RAW/Bband/CM21_LAP.INC"
-
-echo "- - - - - - - - - - - - - - - - - - - -"
-echo "Get signal files 'LAP' of ECO UVA? inclined"
-rsync -arvth                                   \
-    --include '*/'                             \
-    --include '*.LAP'                          \
-    --include '*.lap'                          \
-    --include '*.ori'                          \
-    --exclude '*'                              \
-    "$SOURCE/archive/Bband/EKO_LAP.GLB/"       \
-    "$HOME/DATA_RAW/Bband/EKO_LAP.GLB"
-
-echo "- - - - - - - - - - - - - - - - - - - -"
-echo "Get CHP1 signal files"
-rsync -arvth                                   \
-    "$SOURCE/archive/Bband/CHP1_lap.DIR/"      \
-    "$HOME/DATA_RAW/Bband/CHP1_lap.DIR"
+# echo "- - - - - - - - - - - - - - - - - - - -"
+# echo "Get signal files 'LAP' of CM-21 global"
+# rsync -arvth                                   \
+#     --include '*/'                             \
+#     --include '*.LAP'                          \
+#     --include '*.lap'                          \
+#     --include '*.ori'                          \
+#     --exclude '*'                              \
+#     "$SOURCE/archive/Bband/AC21_LAP.GLB/"      \
+#     "$HOME/DATA_RAW/Bband/AC21_LAP.GLB"
+# 
+# echo "- - - - - - - - - - - - - - - - - - - -"
+# echo "Get signal files 'LAP' of CM-21 inclined"
+# rsync -arvth                                   \
+#     --include '*/'                             \
+#     --include '*.LAP'                          \
+#     --include '*.lap'                          \
+#     --include '*.ori'                          \
+#     --exclude '*'                              \
+#     "$SOURCE/archive/Bband/CM21_LAP.INC/"      \
+#     "$HOME/DATA_RAW/Bband/CM21_LAP.INC"
+# 
+# echo "- - - - - - - - - - - - - - - - - - - -"
+# echo "Get signal files 'LAP' of ECO UVA? inclined"
+# rsync -arvth                                   \
+#     --include '*/'                             \
+#     --include '*.LAP'                          \
+#     --include '*.lap'                          \
+#     --include '*.ori'                          \
+#     --exclude '*'                              \
+#     "$SOURCE/archive/Bband/EKO_LAP.GLB/"       \
+#     "$HOME/DATA_RAW/Bband/EKO_LAP.GLB"
+# 
+# echo "- - - - - - - - - - - - - - - - - - - -"
+# echo "Get CHP1 signal files"
+# rsync -arvth                                   \
+#     "$SOURCE/archive/Bband/CHP1_lap.DIR/"      \
+#     "$HOME/DATA_RAW/Bband/CHP1_lap.DIR"
 
 
 
@@ -74,32 +106,32 @@ rsync -arvth                                   \
 
 echo "- - - - - - - - - - - - - - - - - - - -"
 echo "Get total radiation files 'TOT.DAT'"
-rsync -arvth                                   \
-    --delete                                   \
-    "$SOURCE/products/Bband/AC21_lap.GLB/"     \
-    "$HOME/DATA/cm21_data_validation/AC21_lap.GLB_TOT"
+rsync -arvth                                 \
+  --delete                                   \
+  "$SOURCE/products/Bband/AC21_lap.GLB/"     \
+  "$HOME/DATA/cm21_data_validation/AC21_lap.GLB_TOT"
 
 
 echo "- - - - - - - - - - - - - - - - - - - -"
 echo "get other relative files"
-rsync -arvth                                   \
-    --include '*/'                             \
-    --include '*.txt'                          \
-    --include '*.bas'                          \
-    --include '*.BAS'                          \
-    --include '*.doc'                          \
-    --include '*.docx'                         \
-    --include '*.dat'                          \
-    --include '*.xls'                          \
-    --include '*.xlsx'                         \
-    --exclude '*'                              \
-    --prune-empty-dirs                         \
-    "$SOURCE/process/"                         \
-    "$HOME/DATA/process_sirena/"
+rsync -arvth                                 \
+  --include '*/'                             \
+  --include '*.txt'                          \
+  --include '*.bas'                          \
+  --include '*.BAS'                          \
+  --include '*.doc'                          \
+  --include '*.docx'                         \
+  --include '*.dat'                          \
+  --include '*.xls'                          \
+  --include '*.xlsx'                         \
+  --exclude '*'                              \
+  --prune-empty-dirs                         \
+  "$SOURCE/process/"                         \
+  "$HOME/DATA/process_sirena/"
 
 echo "- - - - - - - - - - - - - - - - - - - -"
 echo "get Brewer files"
-rsync -arvth -u                                 \
+rsync -arvth -u                                \
   "$SOURCE/products/Dsc"                       \
   "$HOME/DATA_RAW/Brewer_005"
 
@@ -108,14 +140,14 @@ echo "- - - - - - - - - - - - - - - - - - - -"
 echo "get Sky camera files"
 ## Don't need to wait for this
 (
-  rsync -arvth                                \
-    --exclude "Thumns.db"                     \
-    "$SOURCE/products/skycam"                 \
-    "/home/single/LAP_skycam/"
-  rsync -arvth                                \
-    --exclude "Thumns.db"                     \
-    "$SOURCE/products/skycam_old"             \
-    "/home/single/LAP_skycam/"
+rsync -arvth                                \
+  --exclude "Thumns.db"                     \
+  "$SOURCE/products/skycam"                 \
+  "/home/single/LAP_skycam/"
+rsync -arvth                                \
+  --exclude "Thumns.db"                     \
+  "$SOURCE/products/skycam_old"             \
+  "/home/single/LAP_skycam/"
 ) &
 
 ##  Commit data to github to preserve manual edits  ----------------------------
