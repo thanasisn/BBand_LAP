339e6a5531a89440ca3dc6402201bf3b503a309e
thanasisn <11601717+thanasisn@users.noreply.github.com>
2025-02-06 10:07:30 +0200

    Commit 2025-02-06 10:07

--- inspect_duckdb/Inspect_BB_DB.R
+++ inspect_duckdb/Inspect_BB_DB.R
@@ -17,6 +17,9 @@ renv::load("~/BBand_LAP", quiet = TRUE)
 
 output_dir <- "~/BBand_LAP/REPORTS/REPORTS/Inspect/"
 dir.create(output_dir, showWarnings = FALSE, recursive = TRUE)
+output_format <- bookdown::pdf_document2()
+
+library(rmarkdown)
 
 ## Run every nth day
 run_days <- 10
@@ -24,58 +27,58 @@ run_days <- 10
 ## __ Check inputs  ------------------------------------------------------------
 try({
   cat("\n\n 0. Check files\n")
-  rmarkdown::render(input       = "~/BBand_LAP/inspect_duckdb/00_Check_input_files.R",
-                    output_dir  = output_dir)
+  render(input       = "~/BBand_LAP/inspect_duckdb/00_Check_input_files.R",
+         output_dir  = output_dir)
 })
 
 try({
   cat("\n\n 1a. Inspect CHP-1 signal CLEAN\n")
-  rmarkdown::render(input       = "~/BBand_LAP/inspect_duckdb/01_Inspect_CHP1_sig_snc_temp.R",
-                    params      = list(CLEAN = TRUE),
-                    output_file = "01_Inspect_CHP1_sig_snc_temp_CLEAN.pdf",
-                    output_dir  = output_dir)
+  render(input       = "~/BBand_LAP/inspect_duckdb/01_Inspect_CHP1_sig_snc_temp.R",
+         params      = list(CLEAN = TRUE),
+         output_file = "01_Inspect_CHP1_sig_snc_temp_CLEAN.pdf",
+         output_dir  = output_dir)
 })
 try({
   cat("\n\n 1b. Inspect CHP-1 signal DIRTY\n")
-  rmarkdown::render(input       = "~/BBand_LAP/inspect_duckdb/01_Inspect_CHP1_sig_snc_temp.R",
-                    params      = list(CLEAN = FALSE),
-                    output_file = "01_Inspect_CHP1_sig_snc_temp_DIRTY.pdf",
-                    output_dir  = output_dir)
+  render(input       = "~/BBand_LAP/inspect_duckdb/01_Inspect_CHP1_sig_snc_temp.R",
+         params      = list(CLEAN = FALSE),
+         output_file = "01_Inspect_CHP1_sig_snc_temp_DIRTY.pdf",
+         output_dir  = output_dir)
 })
 
 
 try({
   cat("\n\n 2a. Inspect CM-21 signal CLEAN\n")
-  rmarkdown::render(input       = "~/BBand_LAP/inspect_duckdb/02_Inspect_CM21_sig.R",
-                    params      = list(CLEAN = TRUE),
-                    output_file = "02_Inspect_CM21_sig_CLEAN.pdf",
-                    output_dir  = output_dir)
+  render(input       = "~/BBand_LAP/inspect_duckdb/02_Inspect_CM21_sig.R",
+         params      = list(CLEAN = TRUE),
+         output_file = "02_Inspect_CM21_sig_CLEAN.pdf",
+         output_dir  = output_dir)
 })
 try({
   cat("\n\n 2b. Inspect CM-21 signal DIRTY\n")
-  rmarkdown::render(input       = "~/BBand_LAP/inspect_duckdb/02_Inspect_CM21_sig.R",
-                    params      = list(CLEAN = FALSE),
-                    output_file = "02_Inspect_CM21_sig_DIRTY.pdf",
-                    output_dir  = output_dir)
+  render(input       = "~/BBand_LAP/inspect_duckdb/02_Inspect_CM21_sig.R",
+         params      = list(CLEAN = FALSE),
+         output_file = "02_Inspect_CM21_sig_DIRTY.pdf",
+         output_dir  = output_dir)
 })
 
 ## No point for clean yet
 try({
   cat("\n\n 3b. Inspect INCLINED CM-21 signal DIRTY\n")
-  rmarkdown::render(input       = "~/BBand_LAP/inspect_duckdb/03_Inspect_CM21INC_sig.R",
-                    params      = list(CLEAN = FALSE),
-                    output_file = "03_Inspect_CM21INC_sig_DIRTY.pdf",
-                    output_dir  = output_dir)
+  render(input       = "~/BBand_LAP/inspect_duckdb/03_Inspect_CM21INC_sig.R",
+         params      = list(CLEAN = FALSE),
+         output_file = "03_Inspect_CM21INC_sig_DIRTY.pdf",
+         output_dir  = output_dir)
 })
 
 if (as.numeric(Sys.Date()) %% run_days == 0)
 {
   try({
     cat("\n\n 4b. Inspect ERPPLAY PIR signal DIRTY\n")
-    rmarkdown::render(input       = "~/BBand_LAP/inspect_duckdb/04_Inspect_PIR_sig.R",
-                      params      = list(CLEAN = FALSE),
-                      output_file = "04_Inspect_PIR_sig_DIRTY.pdf",
-                      output_dir  = output_dir)
+    render(input       = "~/BBand_LAP/inspect_duckdb/04_Inspect_PIR_sig.R",
+           params      = list(CLEAN = FALSE),
+           output_file = "04_Inspect_PIR_sig_DIRTY.pdf",
+           output_dir  = output_dir)
   })
 }
 
@@ -98,14 +101,14 @@ try({
 ## __ Check Radiation  ---------------------------------------------------------
 try({
   cat("\n\n 20. Inspect CHP-1 radiation\n")
-  rmarkdown::render(input      = "~/BBand_LAP/inspect_duckdb/20_Inspect_CHP1_rad_temp.R",
-                    output_dir = output_dir)
+  render(input      = "~/BBand_LAP/inspect_duckdb/20_Inspect_CHP1_rad_temp.R",
+         output_dir = output_dir)
 })
 
 try({
   cat("\n\n 21. Inspect CM-21 radiation\n")
-  rmarkdown::render(input      = "~/BBand_LAP/inspect_duckdb/21_Inspect_CM21_rad.R",
-                    output_dir = output_dir)
+  render(input      = "~/BBand_LAP/inspect_duckdb/21_Inspect_CM21_rad.R",
+         output_dir = output_dir)
 })
 
 
@@ -113,21 +116,23 @@ try({
 # source("~/BBand_LAP/inspect_duckdb/30_Plot_daily_CHP1_L1.R")
 try({
   cat("\n\n 30. Plot daily radiation\n")
-  rmarkdown::render(input      = "~/BBand_LAP/inspect_duckdb/30_Plot_daily_CHP1_L1.R",
-                    output_dir = output_dir)
+  render(input      = "~/BBand_LAP/inspect_duckdb/30_Plot_daily_CHP1_L1.R",
+         output_dir = output_dir)
 })
 # source("~/BBand_LAP/inspect_duckdb/31_Plot_daily_CM21_L1.R")
 try({
   cat("\n\n 31. Plot daily radiation\n")
-  rmarkdown::render(input      = "~/BBand_LAP/inspect_duckdb/31_Plot_daily_CM21_L1.R",
-                    output_dir = output_dir)
+  render(input      = "~/BBand_LAP/inspect_duckdb/31_Plot_daily_CM21_L1.R",
+         output_dir = output_dir)
 })
 
 if (as.numeric(Sys.Date()) %% run_days == 0)
 {
   try({
     cat("\n\n 60. Inspect CM-21/TOT radiation\n")
-    rmarkdown::render(input      = "~/BBand_LAP/inspect_duckdb/60_Inspect_TOT_GLB.R",
-                      output_dir  = output_dir)
+    render(input      = "~/BBand_LAP/inspect_duckdb/60_Inspect_TOT_GLB.R",
+           output_dir  = output_dir)
   })
 }
+
+cat("\n\nEND of inspecting the DB\n\n")
