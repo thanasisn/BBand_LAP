aba3d42c879a2e13a64df1a9bf7f912a971f5fcc
thanasisn <11601717+thanasisn@users.noreply.github.com>
2025-02-06 09:07:54 +0200

    Commit 2025-02-06 09:07

--- build_duckdb/Build_BB_DB.R
+++ build_duckdb/Build_BB_DB.R
@@ -9,6 +9,9 @@ renv::load("~/BBand_LAP", quit = TRUE)
 
 output_dir <- "~/BBand_LAP/REPORTS/REPORTS/Build/"
 dir.create(output_dir, showWarnings = FALSE, recursive = TRUE)
+output_format <- bookdown::pdf_document2()
+
+library(rmarkdown)
 
 # ## Build tracker database  ---------------------------------------------------
 # ## TODO: Under development
@@ -23,50 +26,53 @@ source("~/BBand_LAP/parameters/sun/export_sun_data.R"           )
 ##  Import raw data from instruments  ------------------------------------------
 # source("~/BBand_LAP/build_duckdb/Build_DB_01_initialize.R"      )
 cat("\n\n 01. Build DB initialize\n")
-rmarkdown::render(input      = "~/BBand_LAP/build_duckdb/Build_DB_01_initialize.R",
-                  output_dir = output_dir)
+render(input         = "~/BBand_LAP/build_duckdb/Build_DB_01_initialize.R",
+       output_format = output_format,
+       output_dir    = output_dir)
 
 # source("~/BBand_LAP/build_duckdb/Build_DB_02_cm21.R"            )
 cat("\n\n 02. Build DB cm21\n")
-rmarkdown::render(input      = "~/BBand_LAP/build_duckdb/Build_DB_02_cm21.R",
-                  output_dir = output_dir)
+render(input         = "~/BBand_LAP/build_duckdb/Build_DB_02_cm21.R",
+       output_format = output_format,
+       output_dir    = output_dir)
 
 # source("~/BBand_LAP/build_duckdb/Build_DB_03_chp1.R"            )
 cat("\n\n 03. Build DB chp1\n")
-rmarkdown::render(input      = "~/BBand_LAP/build_duckdb/Build_DB_03_chp1.R",
-                  output_dir = output_dir)
+render(input         = "~/BBand_LAP/build_duckdb/Build_DB_03_chp1.R",
+       output_format = output_format,
+       output_dir    = output_dir)
 
 
 # source("~/BBand_LAP/build_duckdb/Build_DB_04_chp1_SNC.R"        )
 cat("\n\n 04. Build DB chp1 SNC\n")
-rmarkdown::render(input      = "~/BBand_LAP/build_duckdb/Build_DB_04_chp1_SNC.R",
-                  output_dir = output_dir)
+render(input      = "~/BBand_LAP/build_duckdb/Build_DB_04_chp1_SNC.R",
+       output_dir = output_dir)
 
 # source("~/BBand_LAP/build_duckdb/Build_DB_05_chp1_TMP.R"        )
 cat("\n\n 05. Build DB chp1 TMP\n")
-rmarkdown::render(input      = "~/BBand_LAP/build_duckdb/Build_DB_05_chp1_TMP.R",
-                  output_dir = output_dir)
+render(input      = "~/BBand_LAP/build_duckdb/Build_DB_05_chp1_TMP.R",
+       output_dir = output_dir)
 
 # source("~/BBand_LAP/build_duckdb/Build_DB_06_cm21inclined.R"    )
 cat("\n\n 06. Build DB 06 cm21inclined\n")
-rmarkdown::render(input      = "~/BBand_LAP/build_duckdb/Build_DB_06_cm21inclined.R",
-                  output_dir = output_dir)
+render(input      = "~/BBand_LAP/build_duckdb/Build_DB_06_cm21inclined.R",
+       output_dir = output_dir)
 
 # source("~/BBand_LAP/build_duckdb/Build_DB_07_pir.R"             )
 cat("\n\n 07. Build DB pir\n")
-rmarkdown::render(input      = "~/BBand_LAP/build_duckdb/Build_DB_07_pir.R",
-                  output_dir = output_dir)
+render(input      = "~/BBand_LAP/build_duckdb/Build_DB_07_pir.R",
+       output_dir = output_dir)
 
 # source("~/BBand_LAP/build_duckdb/Build_DB_16_cm21_TOT.R"        )
 cat("\n\n 16. Build DB cm21 TOT\n")
-rmarkdown::render(input      = "~/BBand_LAP/build_duckdb/Build_DB_16_cm21_TOT.R",
-                  output_dir = output_dir)
+render(input      = "~/BBand_LAP/build_duckdb/Build_DB_16_cm21_TOT.R",
+       output_dir = output_dir)
 
 ##  Flag bad data  -------------------------------------------------------------
 # source("~/BBand_LAP/build_duckdb/Build_DB_30_exclude_ranges.R"  )
 cat("\n\n 30. Build DB exclude ranges\n")
-rmarkdown::render(input      = "~/BBand_LAP/build_duckdb/Build_DB_30_exclude_ranges.R",
-                  output_dir = output_dir)
+render(input      = "~/BBand_LAP/build_duckdb/Build_DB_30_exclude_ranges.R",
+       output_dir = output_dir)
 
 
 ##  Data processing  -----------------------------------------------------------
@@ -74,13 +80,13 @@ rmarkdown::render(input      = "~/BBand_LAP/build_duckdb/Build_DB_30_exclude_ran
 ## __ Apply dark offset and convert to radiation  ------------------------------
 # source("~/BBand_LAP/build_duckdb/Build_DB_42_cm21_dark_radiat.R")
 cat("\n\n 42. Build DB cm21 dark radiat\n")
-rmarkdown::render(input      = "~/BBand_LAP/build_duckdb/Build_DB_42_cm21_dark_radiat.R",
-                  output_dir = output_dir)
+render(input      = "~/BBand_LAP/build_duckdb/Build_DB_42_cm21_dark_radiat.R",
+       output_dir = output_dir)
 
 # source("~/BBand_LAP/build_duckdb/Build_DB_43_chp1_dark_radiat.R")
 cat("\n\n 43. Build DB chp1 dark radiat\n")
-rmarkdown::render(input      = "~/BBand_LAP/build_duckdb/Build_DB_43_chp1_dark_radiat.R",
-                  output_dir = output_dir)
+render(input      = "~/BBand_LAP/build_duckdb/Build_DB_43_chp1_dark_radiat.R",
+       output_dir = output_dir)
 
 ## __ Second pass to construct missing dark  -----------------------------------
 # source("~/BBand_LAP/build_duckdb/Build_DB_42_cm21_dark_radiat.R")
@@ -93,14 +99,14 @@ rmarkdown::render(input      = "~/BBand_LAP/build_duckdb/Build_DB_43_chp1_dark_r
 # source("~/BBand_LAP/build_duckdb/Build_DB_50_Import_TSI.R"      )
 try({
   cat("\n\n 50. Build DB Import TSI\n")
-  rmarkdown::render(input      = "~/BBand_LAP/build_duckdb/Build_DB_50_Import_TSI.R",
-                    output_dir = output_dir)
+  render(input      = "~/BBand_LAP/build_duckdb/Build_DB_50_Import_TSI.R",
+         output_dir = output_dir)
 })
 # source("~/BBand_LAP/build_duckdb/Build_DB_51_Import_Pressure.R" )
 try({
   cat("\n\n 51. Build DB Import Pressure\n")
-  rmarkdown::render(input      = "~/BBand_LAP/build_duckdb/Build_DB_51_Import_Pressure.R",
-                    output_dir = output_dir)
+  render(input      = "~/BBand_LAP/build_duckdb/Build_DB_51_Import_Pressure.R",
+         output_dir = output_dir)
 })
 
 
@@ -108,8 +114,8 @@ try({
 # source("~/BBand_LAP/build_duckdb/Build_DB_60_Create_strict.R"   )
 try({
   cat("\n\n 60. Build DB Create strict\n")
-  rmarkdown::render(input      = "~/BBand_LAP/build_duckdb/Build_DB_60_Create_strict.R",
-                    output_dir = output_dir)
+  render(input      = "~/BBand_LAP/build_duckdb/Build_DB_60_Create_strict.R",
+         output_dir = output_dir)
 })
 
 source("~/BBand_LAP/build_duckdb/Build_chp1_tracker_DB.R")
--- process/Trends_A/Trends_A_DEFINITIONS.R
+++ process/Trends_A/Trends_A_DEFINITIONS.R
@@ -21,51 +21,77 @@ Cloud_daily_ratio_lim     <- 0.6
 
 
 
-#### dictionary ####
-dictionary <-
-  list(DIR_trnd_A            = "DNI",
-       HOR_trnd_A            = "Dir. Irrad. horizontal plane",
-       DIFF_trnd_A           = "Diffuse. Irrad.",
-       DIFF_trnd_A_mean_anom = "Diffuse. Irrad. Anomaly",
-       GLB_trnd_A            = "GHI",
-       tsi1au_att          = "TSI at 1au",
-       near_tcc_des        = "TCCn deseasonalized",
-       near_tcc_att        = "TCC nearest",
-       bilin_tcc_att       = "TCC bilinear fit",
-       near_tcc_zero_N     = "TCCn zero counts",
-       near_tcc_TN         = "TCCn total counts",
-       near_tcc_NOzero_att = "TCCn without zeros",
-       near_tcc_clear_att  = "TCCn almost completely clear",
-       near_tcc_cloud_att  = "TCCn complementary to almost completely clear",
-       near_tcc_zero_rel   = "TCCn zeros relative to total",
-       ALL                 = "All sky cond.",
-       CLEAR               = "Clear sky cond.",
-       CLOUD               = "Cloudy sky cond.")
+# #### dictionary ####
+# dictionary <-
+#   list(DIR_trnd_A            = "DNI",
+#        DIR_trnd_A_mean_anom  = "DNI Anomaly",
+#        HOR_trnd_A            = "Dir. Irrad. horizontal plane",
+#        HOR_trnd_A_mean_anom  = "Dir. Irrad. horizontal plane Anomaly",
+#        DIFF_trnd_A           = "Diffuse. Irrad.",
+#        DIFF_trnd_A_mean_anom = "Diffuse. Irrad. Anomaly",
+#        GLB_trnd_A            = "GHI",
+#        GLB_trnd_A_mean_anom  = "GHI Anomaly",
+#        tsi1au_att            = "TSI at 1au",
+#        near_tcc_des          = "TCCn deseasonalized",
+#        near_tcc_att          = "TCC nearest",
+#        bilin_tcc_att         = "TCC bilinear fit",
+#        near_tcc_zero_N       = "TCCn zero counts",
+#        near_tcc_TN           = "TCCn total counts",
+#        near_tcc_NOzero_att   = "TCCn without zeros",
+#        near_tcc_clear_att    = "TCCn almost completely clear",
+#        near_tcc_cloud_att    = "TCCn complementary to almost completely clear",
+#        near_tcc_zero_rel     = "TCCn zeros relative to total",
+#        ALL                   = "All sky cond.",
+#        CLEAR                 = "Clear sky cond.",
+#        CLOUD                 = "Cloudy sky cond.")
+#
+# library(stringr)
+# ## Function to translate objects names
+# var_name <- function(x) {
+#   res <- c()
+#   for (ax in x) {
+#     ## get match
+#     amatch <- as.vector(unlist(
+#       dictionary[stringr::str_detect(ax, names(dictionary))]
+#     ))
+#     ## return same if not found
+#     if (is.null(amatch)) {
+#       amatch <- ax
+#     }
+#     ## gather results
+#     res <- c(res, amatch)
+#   }
+#   return(res)
+# }
 
-## Function to translate objects names
-var_name <- function(x) {
-  res <- c()
-  for (ax in x) {
-    ## get match
-    amatch <- as.vector(unlist(
-      dictionary[stringr::str_detect(ax, names(dictionary))]
-    ))
-    ## return same if not found
-    if (is.null(amatch)) {
-      amatch <- ax
-    }
-    ## gather results
-    res <- c(res, amatch)
-  }
-  return(res)
+var_name <- function(type) {
+  switch(type,
+         DIR_trnd_A            = "DNI",
+         DIR_trnd_A_mean_anom  = "DNI Anomaly",
+         HOR_trnd_A            = "Dir. Irrad. horizontal plane",
+         HOR_trnd_A_mean_anom  = "Dir. Irrad. horizontal plane Anomaly",
+         DIFF_trnd_A           = "Diffuse. Irrad.",
+         DIFF_trnd_A_mean_anom = "Diffuse. Irrad. Anomaly",
+         GLB_trnd_A            = "GHI",
+         GLB_trnd_A_mean_anom  = "GHI Anomaly",
+         tsi1au_att            = "TSI at 1au",
+         near_tcc_des          = "TCCn deseasonalized",
+         near_tcc_att          = "TCC nearest",
+         bilin_tcc_att         = "TCC bilinear fit",
+         near_tcc_zero_N       = "TCCn zero counts",
+         near_tcc_TN           = "TCCn total counts",
+         near_tcc_NOzero_att   = "TCCn without zeros",
+         near_tcc_clear_att    = "TCCn almost completely clear",
+         near_tcc_cloud_att    = "TCCn complementary to almost completely clear",
+         near_tcc_zero_rel     = "TCCn zeros relative to total",
+         ALL                   = "All sky cond.",
+         CLEAR                 = "Clear sky cond.",
+         CLOUD                 = "Cloudy sky cond.",
+         Trend_A_DAILY_ALL     = "All sky cond. daily",
+         Trend_A_DAILY_CLEAR   = "Clear sky cond. daily",
+         Trend_A_DAILY_CLOUD   = "Cloudy sky cond. daily",
+         type)
 }
-
-var_name("DIFF_trnd_A_mean_anom")
-
-dictionary[agrep("DIFF_trnd_mean_anom", (names(dictionary)))]
-dictionary[agrep("DIFF_trnd_A_mean_anom", (names(dictionary)))]
-dictionary[agrep("DIFF_trnd_A_anom", (names(dictionary)))]
-
 
 
 coldict <- list(
